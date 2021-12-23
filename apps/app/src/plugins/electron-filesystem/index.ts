af8c4d2c34dd7e4bed395ffb3c10d5d4f589e2a5
Wurielle <noahfenghom@gmail.com>
2021-12-23 13:33:03 +0100

    refactor: better type declaration

--- apps/app/src/plugins/electron-filesystem/index.ts
+++ apps/app/src/plugins/electron-filesystem/index.ts
@@ -1,25 +1,17 @@
 import fs from 'fs/promises';
 import { bridge } from '@izabela/electron-bridger';
 
-export interface ElectronFilesystemInstance {
-  readdir: (path?: string) => Promise<string[]>;
-}
-
-declare global {
-  const ElectronFilesystem: ElectronFilesystemInstance;
-  interface Window {
-    ElectronFilesystem: ElectronFilesystemInstance;
-  }
-}
-
 class ElectronFilesystem {
   readdir(path = './') {
     return fs.readdir(path);
   }
 }
 
-const createElectronFilesystemInstance = (): ElectronFilesystemInstance => {
-  return bridge.new(ElectronFilesystem)();
-};
+declare global {
+  const ElectronFilesystem: ElectronFilesystem;
+  interface Window {
+    ElectronFilesystem: ElectronFilesystem;
+  }
+}
 
-export default createElectronFilesystemInstance();
+export default ((): ElectronFilesystem => bridge.new(ElectronFilesystem)())();
