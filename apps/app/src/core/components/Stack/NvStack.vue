881b26605baf41bea383e1fbd72d40bddb9d0b75
Wurielle <noahfenghom@gmail.com>
2022-04-10 22:58:58 +0200

    feat: add stack component

---
+++ apps/app/src/core/components/Stack/NvStack.vue
@@ -0,0 +1,18 @@
+<template>
+  <st-stack :as="as" v-bind="$props">
+    <slot />
+  </st-stack>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { StStack } from './stack.styled'
+import { props } from './stack.shared'
+
+export default defineComponent({
+  name: 'nv-stack',
+  components: {
+    StStack,
+  },
+  props,
+})
+</script>
---
+++ apps/app/src/core/components/Stack/stack.shared.ts
@@ -0,0 +1,23 @@
+import { ExtractPropTypes, PropType } from 'vue'
+import { Properties } from 'csstype'
+
+const theme = require('@/theme')
+
+export type StackPosition = 'right' | 'center' | 'left' | 'apart'
+export const props = {
+  spacing: {
+    type: Number as PropType<keyof typeof theme.spacing>,
+    default: 3,
+  },
+  align: { type: String as PropType<'stretch' | 'center' | 'start' | 'end'>, default: 'stretch' },
+  justify: {
+    type: String as PropType<'start' | 'center' | 'end' | 'between' | 'around'>,
+    default: 'center',
+  },
+  as: {
+    type: String,
+    default: 'div',
+  },
+}
+
+export type Props = ExtractPropTypes<typeof props>
