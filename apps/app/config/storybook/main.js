3921dcd37a1f3bf675a940e054b3fd44006d79ff
Wurielle <noahfenghom@gmail.com>
2022-04-10 23:11:53 +0200

    feat: add center component

--- apps/app/config/storybook/main.js
+++ apps/app/config/storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
+  layout: 'fullscreen',
   core: {
     builder: 'webpack5',
   },
---
+++ apps/app/src/core/components/Center/NvCenter.vue
@@ -0,0 +1,18 @@
+<template>
+  <st-center :as="as" v-bind="$props">
+    <slot />
+  </st-center>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { StCenter } from './center.styled'
+import { props } from './center.shared'
+
+export default defineComponent({
+  name: 'nv-center',
+  components: {
+    StCenter,
+  },
+  props,
+})
+</script>
