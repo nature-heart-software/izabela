f55e0b70d7b35e098a490106b9b5758cecfce813
Wurielle <Wurielle@users.noreply.github.com>
2022-04-15 10:00:58 +0000

    style: prettier formatting

--- apps/app/src/entities/settings/components/inputs/AudioOutputsSelect.vue
+++ apps/app/src/entities/settings/components/inputs/AudioOutputsSelect.vue
@@ -4,14 +4,23 @@
       <NvText>Play speech on default playback device</NvText>
       <NvSwitch
         :modelValue="$store.getters['settings/persisted'].playSpeechOnDefaultPlaybackDevice"
-        @update:modelValue="(value) => $store.dispatch('settings/setProperty', ['persisted.playSpeechOnDefaultPlaybackDevice', value])
+        @update:modelValue="
+          (value) =>
+            $store.dispatch('settings/setProperty', [
+              'persisted.playSpeechOnDefaultPlaybackDevice',
+              value,
+            ])
         "
       />
     </NvGroup>
-    <NvSelect multiple :multiple-limit="5"
-              :modelValue="$store.getters['settings/persisted'].audioOutputDevices"
-              @update:modelValue="(value) => $store.dispatch('settings/setProperty', ['persisted.audioOutputDevices', value])
-        ">
+    <NvSelect
+      multiple
+      :multiple-limit="5"
+      :modelValue="$store.getters['settings/persisted'].audioOutputDevices"
+      @update:modelValue="
+        (value) => $store.dispatch('settings/setProperty', ['persisted.audioOutputDevices', value])
+      "
+    >
       <template v-for="audioOutputDevice in audioOutputDevices" :key="audioOutputDevice.deviceId">
         <NvOption :label="audioOutputDevice.label" :value="audioOutputDevice.deviceId">
           {{ audioOutputDevice.label }}
@@ -37,14 +46,19 @@ export default defineComponent({
 
   setup() {
     const mediaDevices = ref<MediaDeviceInfo[]>([])
-    const updateMediaDevices = () => navigator.mediaDevices.enumerateDevices()
-      .then((devices) => {
+    const updateMediaDevices = () =>
+      navigator.mediaDevices.enumerateDevices().then((devices) => {
         mediaDevices.value = devices
         return devices
       })
     window.addEventListener('devicechange', updateMediaDevices)
     updateMediaDevices()
-    const audioOutputDevices = computed(() => orderBy(mediaDevices.value.filter((m) => m.kind === 'audiooutput' && m.deviceId !== 'default'), ['label']))
+    const audioOutputDevices = computed(() =>
+      orderBy(
+        mediaDevices.value.filter((m) => m.kind === 'audiooutput' && m.deviceId !== 'default'),
+        ['label'],
+      ),
+    )
     return {
       audioOutputDevices,
     }
