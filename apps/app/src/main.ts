00eac021f94c042c9f3d8bd4fe5ca3e282487385
Wurielle <noahfenghom@gmail.com>
2022-04-23 07:43:30 +0200

    fix: prevent fetch on focus

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -2,14 +2,24 @@ import '@/plugins/debug'
 import '@/styles'
 import '@/teams/messenger/renderer'
 import { createApp } from 'vue'
-import { VueQueryPlugin } from "vue-query"
+import { VueQueryPlugin, VueQueryPluginOptions } from "vue-query"
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+const vueQueryPluginOptions: VueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  },
+}
+
 createApp(App)
   .use(store)
   .use(router)
-  .use(VueQueryPlugin)
+  .use(VueQueryPlugin, vueQueryPluginOptions)
   .mount('#app')
