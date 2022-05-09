9f812bf0cea22fd8814265144ccaf2fa31eb4a89
Wurielle <noahfenghom@gmail.com>
2022-05-09 19:10:24 +0200

    fix: fix spacing

--- apps/app/src/entities/settings/components/SettingsAudio.vue
+++ apps/app/src/entities/settings/components/SettingsAudio.vue
@@ -3,30 +3,32 @@
     <NvStack>
       <NvText type="subtitle">Audio</NvText>
       <NvStack spacing="4">
-        <NvCard>
-          <NvStack>
-            <NvText type="label">Audio Outputs</NvText>
-            <NvText>Select audio outputs where Izabela's speech should be played on</NvText>
-          </NvStack>
-        </NvCard>
-        <div class="pl-8">
+        <NvStack spacing="4">
           <NvCard>
-            <AudioOutputsSelect />
+            <NvStack>
+              <NvText type="label">Audio Outputs</NvText>
+              <NvText>Select audio outputs where Izabela's speech should be played on</NvText>
+            </NvStack>
           </NvCard>
-        </div>
-      </NvStack>
-      <NvStack spacing="4">
-        <NvCard>
-          <NvStack>
-            <NvText type="label">Audio Input</NvText>
-            <NvText>Select an audio input to generate Izabela's speech from</NvText>
-          </NvStack>
-        </NvCard>
-        <div class="pl-8">
+          <div class="pl-8">
+            <NvCard>
+              <AudioOutputsSelect />
+            </NvCard>
+          </div>
+        </NvStack>
+        <NvStack spacing="4">
           <NvCard>
-            <AudioInputSelect />
+            <NvStack>
+              <NvText type="label">Audio Input</NvText>
+              <NvText>Select an audio input to generate Izabela's speech from</NvText>
+            </NvStack>
           </NvCard>
-        </div>
+          <div class="pl-8">
+            <NvCard>
+              <AudioInputSelect />
+            </NvCard>
+          </div>
+        </NvStack>
       </NvStack>
     </NvStack>
   </NvStack>
