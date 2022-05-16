aee9fb28754f4fffea37556d4471b9b0d1df1344
Wurielle <noahfenghom@gmail.com>
2022-05-16 07:45:45 +0200

    fix: keep old json import

--- apps/app/src/teams/tray/electron-tray.ts
+++ apps/app/src/teams/tray/electron-tray.ts
@@ -1,14 +1,16 @@
 import { app, Menu, Tray } from 'electron'
 import path from 'path'
+import { readFileSync } from 'fs'
 import ElectronMessengerWindow from '@/entities/messenger/modules/electron-messenger-window'
-import pkg from '@package'
 
 let tray = null
 const createTray = (): Promise<Tray> =>
   app.whenReady().then(() => {
+    const rootPath = process.env.NODE_ENV === 'production' ? __static : path.resolve()
+    const pkg = JSON.parse(readFileSync(path.join(rootPath, 'package.json'), 'utf8'))
     tray = new Tray(path.join(__static, 'icons/256x256.png'))
     const contextMenu = Menu.buildFromTemplate([{ label: 'Exit', type: 'normal', role: 'quit' }])
-    tray.setToolTip(`${ pkg.productName } - v${ pkg.version }`)
+    tray.setToolTip(`${pkg.productName} - v${pkg.version}`)
     tray.setContextMenu(contextMenu)
     tray.on('click', () => {
       ElectronMessengerWindow.show()
