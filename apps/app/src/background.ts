de7fbba4a139369ac6110597fae9f1b09a483e1c
Wurielle <Wurielle@users.noreply.github.com>
2022-05-18 21:04:14 +0000

    style: prettier formatting

--- apps/app/src/background.ts
+++ apps/app/src/background.ts
@@ -15,7 +15,6 @@ import path from 'path'
 import ElectronWindowManager from '@/modules/electron-window-manager'
 import { createMessengerWindow } from '@/teams/messenger/background'
 import { createSpeechWorkerWindow } from '@/teams/speech-worker/background'
-
 ;(() => {
   if (process.platform === 'win32') app.setAppUserModelId(app.name)
   app.commandLine.appendSwitch('disable-renderer-backgrounding')
