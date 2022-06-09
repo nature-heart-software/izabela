b4114b0f9a3508cf7a4867eff0d7340acea7b270
Wurielle <noahfenghom@gmail.com>
2022-06-09 19:23:43 +0200

    feat: add log when fallback is called

--- apps/app/src/electron/background.ts
+++ apps/app/src/electron/background.ts
@@ -1,10 +1,18 @@
 /* eslint global-require: 0 */
 import { autoUpdater } from 'electron-updater'
+import { app } from 'electron'
 
 const fallback = async () => {
+  const version = app.getVersion()
+  const channelPart = version.split('-')[1]
+  const channel = channelPart ? channelPart.split('.')[0] : 'latest'
+  autoUpdater.channel = channel
   autoUpdater.on('update-downloaded', () => {
     autoUpdater.quitAndInstall()
   })
+  console.log(
+    `[system] A critical error occured. Proceeding to update the application on the "${channel}" channel.`,
+  )
   return autoUpdater.checkForUpdates()
 }
 
