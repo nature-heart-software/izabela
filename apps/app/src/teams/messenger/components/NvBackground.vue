cb1ca42821d39d1c5f42a14035d198d15345d71e
Wurielle <Wurielle@users.noreply.github.com>
2022-07-26 21:00:11 +0000

    style: prettier formatting

--- apps/app/src/teams/messenger/components/NvBackground.vue
+++ apps/app/src/teams/messenger/components/NvBackground.vue
@@ -1,7 +1,6 @@
 <template>
   <Transition class="transition">
-    <DomBoundary v-if="isBackgroundShown"
-                 class="absolute inset-0" @click="isWindowShowing = false">
+    <DomBoundary v-if="isBackgroundShown" class="absolute inset-0" @click="isWindowShowing = false">
       <div class="w-full h-full bg-black bg-opacity-20"></div>
     </DomBoundary>
   </Transition>
@@ -13,7 +12,9 @@ import { computed, ref, watch } from 'vue'
 
 const store = useStore()
 const isWindowShowing = ref(false)
-const isBackgroundShown = computed(() => (isWindowShowing.value) || store.state.messenger.isInputFocused)
+const isBackgroundShown = computed(
+  () => isWindowShowing.value || store.state.messenger.isInputFocused,
+)
 
 watch(store.state.messenger.isShown, () => {
   if (store.state.messenger.isShown) {
