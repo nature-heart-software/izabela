718726490036e10f71d55a2c0b767ae29a1b5a8c
Wurielle <noahfenghom@gmail.com>
2022-08-10 11:21:58 +0200

    feat: improve electron-bridger types

--- libs/electron-bridger/src/main.ts
+++ libs/electron-bridger/src/main.ts
@@ -1,12 +1,17 @@
 // @ts-ignore
 import { contextBridge, ipcMain, ipcRenderer } from 'electron'
+import { Callback, Plugin } from './types'
 
+export * from './types'
 export const isRenderer = typeof window !== 'undefined'
 
 export const Bridge = () => {
-  const registeredInstances = {}
+  const registeredInstances: Record<string, ReturnType<Callback<any>>> = {}
 
-  const newInstancInRenderer = (name, pluginCallback) => {
+  const newInstancInRenderer = <C extends Callback<C>>(
+    name: string,
+    pluginCallback: C,
+  ) => {
     const instance = pluginCallback()
     const functionList = Object.entries(instance).filter(
       ([_, value]) => typeof value === 'function',
@@ -16,7 +21,7 @@ export const Bridge = () => {
     }
     for (const [functionName] of functionList) {
       if (!registeredInstances[name][functionName]) {
-        registeredInstances[name][functionName] = (...args) => {
+        registeredInstances[name][functionName] = (...args: any[]) => {
           return ipcRenderer.invoke(`${name}-${functionName}`, ...args)
         }
       }
@@ -27,26 +32,34 @@ export const Bridge = () => {
     return registeredInstances[name]
   }
 
-  const newInstancInMain = (name, pluginCallback) => {
-    const instance = pluginCallback()
+  const newInstancInMain = <C extends Callback<C>>(
+    name: string,
+    pluginCallback: C,
+  ) => {
+    const instance: ReturnType<Callback<any>> = pluginCallback()
     const functionList = Object.entries(instance).filter(
       ([_, value]) => typeof value === 'function',
     )
     for (const [functionName] of functionList) {
-      ipcMain.handle(`${name}-${functionName}`, async (_, ...args) => {
+      ipcMain.handle(`${name}-${functionName}`, async (_, ...args: any[]) => {
         return await instance[functionName](...args)
       })
     }
     return instance
   }
 
-  const registerPlugin = (name, pluginCallback) => {
+  const registerPlugin = <C extends Callback<C>>(
+    name: string,
+    pluginCallback: C,
+  ) => {
     if (isRenderer) {
       return newInstancInRenderer(name, pluginCallback)
     }
     return newInstancInMain(name, pluginCallback)
   }
-  const register = (plugins) => {
+  const register = <P extends Plugin<any>[]>(
+    plugins: Plugin<ReturnType<P[number][1]>>[],
+  ) => {
     plugins.forEach(([name, pluginCallback]) => {
       registerPlugin(name, pluginCallback)
     })
