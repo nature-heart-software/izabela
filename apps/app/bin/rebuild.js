9ec3a152375e0e03bfbb6b98baab2e1aaaeca01b
Wurielle <Wurielle@users.noreply.github.com>
2022-08-19 17:25:02 +0000

    style: prettier formatting

--- apps/app/bin/rebuild.js
+++ apps/app/bin/rebuild.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-const { exec } = require('child_process');
+const { exec } = require('child_process')
 
 exec('npm run rebuild', (error, stdout, stderr) => {
   if (error) {
-    console.log(`error: ${error.message}`);
-    return;
+    console.log(`error: ${error.message}`)
+    return
   }
   if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
+    console.log(`stderr: ${stderr}`)
+    return
   }
-  console.log(`stdout: ${stdout}`);
+  console.log(`stdout: ${stdout}`)
 })
