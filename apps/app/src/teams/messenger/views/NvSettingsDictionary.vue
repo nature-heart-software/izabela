400bffb076c1779d55c6da41024d0138815aaec2
Wurielle <Wurielle@users.noreply.github.com>
2022-08-21 13:31:32 +0000

    style: prettier formatting

--- apps/app/src/teams/messenger/views/NvSettingsDictionary.vue
+++ apps/app/src/teams/messenger/views/NvSettingsDictionary.vue
@@ -15,33 +15,38 @@
           <NvCard>
             <NvStack spacing="5">
               <div>
-                <NvButton size="sm" @click="store.dispatch('dictionary/addDefinition')">Add definition</NvButton>
+                <NvButton size="sm" @click="store.dispatch('dictionary/addDefinition')"
+                  >Add definition</NvButton
+                >
               </div>
               <NvDivider direction="horizontal" />
               <NvGroup class="w-full" grow no-wrap>
                 <NvText type="label" class="w-1/2">Word</NvText>
                 <NvDivider direction="vertical" class="!grow-0 h-5" />
                 <NvText type="label" class="w-1/2">Definition</NvText>
-                <NvButton
-                  class="!grow-0 invisible"
-                  type="plain"
-                  icon-name="times"
-                  size="xs"
-                />
+                <NvButton class="!grow-0 invisible" type="plain" icon-name="times" size="xs" />
               </NvGroup>
               <template v-for="(definition, i) in store.getters['dictionary/definitions']" :key="i">
                 <NvGroup class="w-full" grow>
                   <NvInput
                     :modelValue="definition[0]"
                     @update:modelValue="
-                      (value) => store.dispatch('dictionary/setProperty', [`persisted.definitions[${i}][0]`, value])
+                      (value) =>
+                        store.dispatch('dictionary/setProperty', [
+                          `persisted.definitions[${i}][0]`,
+                          value,
+                        ])
                     "
                   />
                   <NvDivider direction="vertical" class="!grow-0 h-5" />
                   <NvInput
                     :modelValue="definition[1]"
                     @update:modelValue="
-                      (value) => store.dispatch('dictionary/setProperty', [`persisted.definitions[${i}][1]`, value])
+                      (value) =>
+                        store.dispatch('dictionary/setProperty', [
+                          `persisted.definitions[${i}][1]`,
+                          value,
+                        ])
                     "
                   />
                   <NvButton
