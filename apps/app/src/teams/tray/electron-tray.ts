7db6957b7b57e460bb71ff29740bf1448ff72b14
Wurielle <noahfenghom@gmail.com>
2022-08-24 11:26:46 +0200

    feat: add display selection on tray

--- apps/app/src/teams/tray/electron-tray.ts
+++ apps/app/src/teams/tray/electron-tray.ts
@@ -1,32 +1,54 @@
 import { app, Menu, Tray, screen } from 'electron'
 import path from 'path'
 import electronMessengerWindow from '@/teams/messenger/modules/electron-messenger-window'
+import store from '@/store'
 
-let tray = null
+let tray: Tray|null = null
 const createTray = (): Promise<Tray> =>
   app.whenReady().then(() => {
     tray = new Tray(path.join(__static, 'icons/256x256.png'))
-    const contextMenu = Menu.buildFromTemplate([
-      {
-        label: 'Settings',
-        submenu: [
-          {
-            label: 'Reset Display',
-            click: () => {
-              const primaryDisplay = screen.getPrimaryDisplay()
-              electronMessengerWindow.setDisplay(primaryDisplay.id)
+    const getContextMenu = () => {
+      const primaryDisplay = screen.getPrimaryDisplay()
+      const allDisplays = screen.getAllDisplays()
+      return Menu.buildFromTemplate([
+        {
+          label: 'Settings',
+          submenu: [
+            {
+              label: 'Display',
+              submenu: allDisplays.map(({ id }) => ({
+                label: `${id === primaryDisplay.id && '(Primary) ' || ''}${id}`,
+                type: 'radio',
+                checked: id === store.getters['settings/persisted'].display,
+                click: () => {
+                  store.dispatch('settings/setProperty', ['persisted.display', id])
+                  electronMessengerWindow.setDisplay(id)
+                  updateContextMenu()
+                }
+              }))
+            },
+            {
+              label: 'Reset Display',
+              click: () => {
+                store.dispatch('settings/setProperty', ['persisted.display', null])
+                electronMessengerWindow.setDisplay(primaryDisplay.id)
+                updateContextMenu()
+              }
             }
-          }
-        ]
-      },
-      { type: 'separator' },
-      { label: 'Exit', type: 'normal', role: 'quit' }
-    ])
+          ]
+        },
+        { type: 'separator' },
+        { label: 'Exit', type: 'normal', role: 'quit' }
+      ])
+    }
+    function updateContextMenu() {
+      console.log('updateContextMenu', store.getters['settings/persisted'].display)
+      tray?.setContextMenu(getContextMenu())
+    }
     tray.setToolTip(`${app.name} - v${app.getVersion()}`)
-    tray.setContextMenu(contextMenu)
-    tray.on('click', () => {
-      electronMessengerWindow.show()
-    })
+    tray.setContextMenu(getContextMenu())
+    tray.on('click', electronMessengerWindow.show)
+    store.getters.isReady().then(updateContextMenu)
     return tray
   })
 
