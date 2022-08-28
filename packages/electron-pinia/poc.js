cd7ff741fb29b8eafa2a36587d696009ebdda996
Wurielle <Wurielle@users.noreply.github.com>
2022-08-28 18:49:36 +0000

    style: prettier formatting

--- packages/electron-pinia/poc.js
+++ packages/electron-pinia/poc.js
@@ -1,76 +1,76 @@
-const { createApp, h } = require("vue");
-const { createPinia, defineStore } = require("pinia");
-const mitt = require("mitt");
-const { watch } = require("@vue-reactivity/watch");
+const { createApp, h } = require('vue')
+const { createPinia, defineStore } = require('pinia')
+const mitt = require('mitt')
+const { watch } = require('@vue-reactivity/watch')
 
-const emitter = mitt();
-const stores = new Map();
+const emitter = mitt()
+const stores = new Map()
 
 const plugin = ({ store }) => {
-  stores.set(store.$id, store);
+  stores.set(store.$id, store)
   store.$onAction(({ name, store, args }) => {
-    if (store.$id === "main") {
+    if (store.$id === 'main') {
       console.log(
-        `[${store.$id}] Start "${name}" with params [${args.join(", ")}].`
-      );
-      emitter.emit("action", {
+        `[${store.$id}] Start "${name}" with params [${args.join(', ')}].`,
+      )
+      emitter.emit('action', {
         name,
         storeId: store.$id,
-        args
-      });
+        args,
+      })
     } else {
       console.log(
         `[${store.$id}] Start "${name}" with params [${args
           .map(JSON.stringify)
-          .join(", ")}].`
-      );
+          .join(', ')}].`,
+      )
     }
-  });
+  })
 }
 
-createApp(h({})).use(createPinia().use(plugin));
+createApp(h({})).use(createPinia().use(plugin))
 
 const storeState = {
-  state: () => ({ count: 0, name: "Eduardo" }),
+  state: () => ({ count: 0, name: 'Eduardo' }),
   getters: {
-    doubleCount: (state) => state.count * 2
+    doubleCount: (state) => state.count * 2,
   },
   actions: {
     increment() {
-      this.count++;
-    }
-  }
-};
+      this.count++
+    },
+  },
+}
 
 // Both must be in sync
-const useMainStore = defineStore("main", storeState);
-const useRendererStore = defineStore("renderer", storeState);
+const useMainStore = defineStore('main', storeState)
+const useRendererStore = defineStore('renderer', storeState)
 
-const mainStore = useMainStore();
-const rendererStore = useRendererStore();
-
-setInterval(() => {
-  console.log("-------------------");
-  console.log("[main]", mainStore.count);
-  console.log("[renderer]", rendererStore.count);
-}, 1000);
+const mainStore = useMainStore()
+const rendererStore = useRendererStore()
 
 setInterval(() => {
-  mainStore.increment();
-}, 1000);
+  console.log('-------------------')
+  console.log('[main]', mainStore.count)
+  console.log('[renderer]', rendererStore.count)
+}, 1000)
 
-emitter.on("action", ({ name, storeId, args }) => {
+setInterval(() => {
+  mainStore.increment()
+}, 1000)
+
+emitter.on('action', ({ name, storeId, args }) => {
   // in other process
   // const store = stores.get(storeId);
-  const store = stores.get("renderer");
-  if (store && store[name] && typeof store[name] === "function") {
-    store[name](...args, { trigger: "sync" });
+  const store = stores.get('renderer')
+  if (store && store[name] && typeof store[name] === 'function') {
+    store[name](...args, { trigger: 'sync' })
   }
-});
+})
 
 watch(
   () => rendererStore.doubleCount,
   () => {
-    console.log("Reactivity still works");
-  }
-);
+    console.log('Reactivity still works')
+  },
+)
