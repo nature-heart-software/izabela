6751e949c8e39fec815c0e69d207da210b61b3c4
Wurielle <Wurielle@users.noreply.github.com>
2022-08-29 20:35:18 +0000

    style: prettier formatting

--- packages/electron-pinia/src/main.ts
+++ packages/electron-pinia/src/main.ts
@@ -3,7 +3,6 @@ import { AugmentedGlobal } from './types'
 import { plugin } from './plugin'
 import { ELECTRON_STORAGE_NAME } from './consts'
 import ElectronStore from 'electron-store'
-
 ;(global as AugmentedGlobal).ElectronPiniaStorage = new ElectronStore({
   name: ELECTRON_STORAGE_NAME,
 })
