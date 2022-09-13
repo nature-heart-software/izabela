83d4a250ccfa94279a9443d8cc8cbd1b566d5905
Wurielle <Wurielle@users.noreply.github.com>
2022-09-13 18:51:03 +0000

    style: prettier formatting

--- apps/app/src/plugins/speech-engines/uberduck/hooks.ts
+++ apps/app/src/plugins/speech-engines/uberduck/hooks.ts
@@ -2,9 +2,7 @@ import { useQuery, UseQueryOptions } from 'vue-query'
 import { api } from '@/services'
 import { LIST_VOICES_QUERY_KEY } from './consts'
 
-export const useListVoicesQuery = (
-  options?: UseQueryOptions,
-) =>
+export const useListVoicesQuery = (options?: UseQueryOptions) =>
   useQuery<any>(
     LIST_VOICES_QUERY_KEY,
     () => api.post('/tts/uberduck/list-voices').then(({ data }) => data),
