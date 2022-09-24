47ab51b66c378f0905f90766e4a43d243d2eb88d
Wurielle <Wurielle@users.noreply.github.com>
2022-09-24 22:38:43 +0000

    style: prettier formatting

--- packages/ui/src/components/forms/Select/select.stories.ts
+++ packages/ui/src/components/forms/Select/select.stories.ts
@@ -6,36 +6,36 @@ import { props, sizeValues } from './select.shared'
 import voices from '@/mocks/voices.json'
 
 export default {
-    title: 'Select',
-    component: NvSelect,
-    argTypes: {
-        size: {
-            defaultValue: props.size.default,
-            control: 'inline-radio',
-            options: sizeValues,
-        },
-        placeholder: {
-            defaultValue: 'Select some options...',
-            control: 'text',
-        },
+  title: 'Select',
+  component: NvSelect,
+  argTypes: {
+    size: {
+      defaultValue: props.size.default,
+      control: 'inline-radio',
+      options: sizeValues,
     },
+    placeholder: {
+      defaultValue: 'Select some options...',
+      control: 'text',
+    },
+  },
 }
 
 const Template: Story = (args) => ({
-    components: { NvSelect, NvCenter },
-    setup() {
-        return {
-            args,
-            inputValue: ref(args.inputValue),
-            options: ref(
-                voices.map((voice) => ({
-                    value: voice,
-                    label: voice.display_name,
-                })),
-            ),
-        }
-    },
-    template: `
+  components: { NvSelect, NvCenter },
+  setup() {
+    return {
+      args,
+      inputValue: ref(args.inputValue),
+      options: ref(
+        voices.map((voice) => ({
+          value: voice,
+          label: voice.display_name,
+        })),
+      ),
+    }
+  },
+  template: `
       <NvCenter :style="{height: '200px'}">
       <NvSelect v-model="inputValue" v-bind="args" :options="options"/>
       </NvCenter>`,
@@ -43,13 +43,13 @@ const Template: Story = (args) => ({
 
 export const Default = Template.bind({})
 Default.args = {
-    valueKey: 'voicemodel_uuid',
-    labelKey: 'display_name',
+  valueKey: 'voicemodel_uuid',
+  labelKey: 'display_name',
 }
 export const Multiple = Template.bind({})
 Multiple.args = {
-    valueKey: 'voicemodel_uuid',
-    labelKey: 'display_name',
-    multiple: true,
-    inputValue: [],
+  valueKey: 'voicemodel_uuid',
+  labelKey: 'display_name',
+  multiple: true,
+  inputValue: [],
 }
