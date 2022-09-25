d4491024598f96f902d77ee28194a43390b8075c
Wurielle <noahfenghom@gmail.com>
2022-09-25 23:05:17 +0200

    feat: improve background color display clarity

--- apps/app/src/teams/messenger/components/NvBackground.vue
+++ apps/app/src/teams/messenger/components/NvBackground.vue
@@ -7,21 +7,9 @@
 </template>
 <script lang="ts" setup>
 import DomBoundary from '@/modules/vue-dom-boundaries/DomBoundary.vue'
-import { computed, ref, watch } from 'vue'
+import { computed } from 'vue'
 import { useMessengerWindowStore } from '@/teams/messenger/store'
 
 const messengerWindowStore = useMessengerWindowStore()
-const isWindowShowing = ref(false)
-const isBackgroundShown = computed(
-  () => isWindowShowing.value || messengerWindowStore.isInputFocused,
-)
-
-watch(
-  () => messengerWindowStore.isShown,
-  () => {
-    if (messengerWindowStore.isShown) {
-      isWindowShowing.value = true
-    }
-  },
-)
+const isBackgroundShown = computed(() => messengerWindowStore.isInputFocused)
 </script>
