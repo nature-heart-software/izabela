cf766c28876584bea6f90b8422e4b3292b9b18b7
Wurielle <Wurielle@users.noreply.github.com>
2022-10-02 20:45:17 +0000

    style: prettier formatting

--- packages/ui/src/components/forms/Button/button.stories.ts
+++ packages/ui/src/components/forms/Button/button.stories.ts
@@ -4,50 +4,50 @@ import { props, sizeValues, typeValues } from './button.shared'
 import { NvButton } from '@/components'
 
 export default {
-    title: 'Button',
-    component: NvButton,
-    argTypes: {
-        content: {
-            defaultValue: 'hello world',
-            control: 'text',
-        },
-        type: {
-            defaultValue: props.type.default,
-            options: typeValues,
-            control: 'inline-radio',
-        },
-        size: {
-            defaultValue: props.size.default,
-            options: sizeValues,
-            control: 'inline-radio',
-        },
-        selected: {
-            defaultValue: props.selected.default,
-            control: 'boolean',
-        },
-        squared: {
-            defaultValue: props.squared.default,
-            control: 'boolean',
-        },
-        loading: {
-            defaultValue: props.loading.default,
-            control: 'boolean',
-        },
-        iconName: {
-            defaultValue: '',
-            control: 'text',
-        },
+  title: 'Button',
+  component: NvButton,
+  argTypes: {
+    content: {
+      defaultValue: 'hello world',
+      control: 'text',
     },
+    type: {
+      defaultValue: props.type.default,
+      options: typeValues,
+      control: 'inline-radio',
+    },
+    size: {
+      defaultValue: props.size.default,
+      options: sizeValues,
+      control: 'inline-radio',
+    },
+    selected: {
+      defaultValue: props.selected.default,
+      control: 'boolean',
+    },
+    squared: {
+      defaultValue: props.squared.default,
+      control: 'boolean',
+    },
+    loading: {
+      defaultValue: props.loading.default,
+      control: 'boolean',
+    },
+    iconName: {
+      defaultValue: '',
+      control: 'text',
+    },
+  },
 }
 
 const Template: Story = (args) => ({
-    components: { NvButton },
-    setup() {
-        return {
-            args,
-        }
-    },
-    template: `
+  components: { NvButton },
+  setup() {
+    return {
+      args,
+    }
+  },
+  template: `
       <NvButton v-bind="args">{{ args.content }}</NvButton>
     `,
 })
@@ -57,25 +57,25 @@ Default.args = {}
 
 export const Plain = Template.bind({})
 Plain.args = {
-    type: 'plain',
+  type: 'plain',
 }
 
 export const Ghost = Template.bind({})
 Ghost.args = {
-    type: 'ghost',
+  type: 'ghost',
 }
 export const GhostAlt = Template.bind({})
 GhostAlt.args = {
-    type: 'ghost-alt',
+  type: 'ghost-alt',
 }
 
 export const WithIcon = Template.bind({})
 WithIcon.args = {
-    iconName: 'github',
+  iconName: 'github',
 }
 
 export const WithIconOnly = Template.bind({})
 WithIconOnly.args = {
-    iconName: 'github',
-    content: '',
+  iconName: 'github',
+  content: '',
 }
