7567a5bea0f01ba9bd24bc0362fc3f84a7dc4b1e
Wurielle <Wurielle@users.noreply.github.com>
2022-10-12 19:38:01 +0000

    style: prettier formatting

--- apps/app/src/features/speech/components/inputs/NvSpeechEngineInput.vue
+++ apps/app/src/features/speech/components/inputs/NvSpeechEngineInput.vue
@@ -75,7 +75,7 @@ const commands = computed(
   () =>
     engine.value?.commands?.(props.voice).map((command) => ({
       ...command,
-      command: `/${ command.value }`,
+      command: `/${command.value}`,
     })) || [],
 )
 
@@ -117,7 +117,7 @@ const isAutocompleteVisible = computed(
 )
 
 const onAutocompleteSelect = (value: typeof commands.value[number]) => {
-  emit('update:modelValue', `${ value.command } `)
+  emit('update:modelValue', `${value.command} `)
   if (latestCommands.value.includes(value.command)) {
     latestCommands.value.splice(latestCommands.value.indexOf(value.command), 1)
   }
@@ -147,7 +147,7 @@ onKeyStroke('ArrowUp', () => {
   if (
     !isAutocompleteVisible.value &&
     isInputFocused.value &&
-    historyMessageIndex.value < messagesStore.history.length-1
+    historyMessageIndex.value < messagesStore.history.length - 1
   ) {
     historyMessageIndex.value += 1
   }
@@ -159,23 +159,35 @@ onKeyStroke('ArrowDown', () => {
   }
 })
 
-const onInputEnter = throttle((e: KeyboardEvent) => {
-  if (!isAutocompleteVisible.value) {
-    emit('enter', e)
-  }
-}, 40, { leading: true, trailing: false })
+const onInputEnter = throttle(
+  (e: KeyboardEvent) => {
+    if (!isAutocompleteVisible.value) {
+      emit('enter', e)
+    }
+  },
+  40,
+  { leading: true, trailing: false },
+)
 
-const onInputSpace = throttle((e: KeyboardEvent) => {
-  if (!isAutocompleteVisible.value) {
-    emit('space', e)
-  }
-}, 40, { leading: true, trailing: false })
+const onInputSpace = throttle(
+  (e: KeyboardEvent) => {
+    if (!isAutocompleteVisible.value) {
+      emit('space', e)
+    }
+  },
+  40,
+  { leading: true, trailing: false },
+)
 
-const onInputEsc = throttle((e: KeyboardEvent) => {
-  if (!isAutocompleteVisible.value) {
-    emit('esc', e)
-  }
-}, 40, { leading: true, trailing: false })
+const onInputEsc = throttle(
+  (e: KeyboardEvent) => {
+    if (!isAutocompleteVisible.value) {
+      emit('esc', e)
+    }
+  },
+  40,
+  { leading: true, trailing: false },
+)
 
 const onInputFocus = () => {
   isInputFocused.value = true
--- apps/app/src/teams/messenger/components/NvShortcutMessage.vue
+++ apps/app/src/teams/messenger/components/NvShortcutMessage.vue
@@ -20,7 +20,7 @@
               @enter="() => play()"
             />
             <NvGroup noWrap>
-              <NvSpeechEngineSelect v-model="data.engine" class="w-1/3" size="sm"/>
+              <NvSpeechEngineSelect v-model="data.engine" class="w-1/3" size="sm" />
               <template v-if="engine">
                 <component
                   :is="engine.voiceSelectComponent"
@@ -31,7 +31,7 @@
                   size="sm"
                 />
               </template>
-              <NvKeybinding v-model="data.shortcut" class="w-1/3" multiple size="sm"/>
+              <NvKeybinding v-model="data.shortcut" class="w-1/3" multiple size="sm" />
             </NvGroup>
           </NvStack>
         </NvGroup>
@@ -46,7 +46,7 @@
             },
           ]"
         >
-          <NvButton class="shrink-0" icon-name="ellipsis-v" size="sm"/>
+          <NvButton class="shrink-0" icon-name="ellipsis-v" size="sm" />
         </NvContextMenu>
       </NvGroup>
       <div v-if="isPlaying" class="h-2 relative bg-gray-10">
@@ -112,7 +112,7 @@ const commands = computed(
   () =>
     engine.value?.commands?.(data.selectedVoice[engine.value.id]).map((command) => ({
       ...command,
-      command: `/${ command.value }`,
+      command: `/${command.value}`,
     })) || [],
 )
 
@@ -145,7 +145,7 @@ const isAutocompleteVisible = computed(
 )
 
 const onAutocompleteSelect = (value: typeof commands.value[number]) => {
-  data.message = `${ value.command } `
+  data.message = `${value.command} `
 }
 
 watch(
