22e0a36d9d0ec2afa75b3ab805bf7b329f24b973
Wuriko <noahfenghom@gmail.com>
2023-01-28 16:42:04 +0100

    feat: add izabela tts engine

---
+++ apps/app/src/plugins/speech-engines/izabela/shared.ts
@@ -0,0 +1,4 @@
+export const ENGINE_ID = 'izabelatts' as const
+export const ENGINE_NAME = 'Izabela' as const
+export const LIST_VOICES_QUERY_KEY = 'izabelatts-list-voices' as const
+export const getVoiceName = (voice: any) => voice.name
