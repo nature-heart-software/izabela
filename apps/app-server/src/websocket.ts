82da2a2f25c6da0b013cf8f1ae2e11a8dabdc84b
Wurielle <Wurielle@users.noreply.github.com>
2023-01-28 13:04:33 +0000

    style: prettier formatting

--- apps/app-server/src/websocket.ts
+++ apps/app-server/src/websocket.ts
@@ -1,31 +1,36 @@
 import { Server } from 'socket.io'
 
 export default (() => {
-    let io: Server | null = null
-    const forwardEvent = (event: string, message: any) => {
-        if (io) {
-            io.emit(event, message)
-        }
+  let io: Server | null = null
+  const forwardEvent = (event: string, message: any) => {
+    if (io) {
+      io.emit(event, message)
     }
+  }
 
-    return {
-        start({ port }: { port: number }) {
-            io = new Server(port, {
-                cors: {
-                    origin: '*',
-                    methods: ['*'],
-                },
-            })
-            io.on('connect', (socket) => {
-                console.log('[socket.io] Connected:', socket.id)
-                socket.on('disconnect', () => {
-                    console.log('[socket.io] Disconnected:', socket.id)
-                })
-                ;['message:load', 'message:start', 'message:end', 'message:error'].forEach((event) => {
-                    socket.on(event, (message) => forwardEvent(event, message))
-                })
-            })
+  return {
+    start({ port }: { port: number }) {
+      io = new Server(port, {
+        cors: {
+          origin: '*',
+          methods: ['*'],
         },
-        forwardEvent,
-    }
+      })
+      io.on('connect', (socket) => {
+        console.log('[socket.io] Connected:', socket.id)
+        socket.on('disconnect', () => {
+          console.log('[socket.io] Disconnected:', socket.id)
+        })
+        ;[
+          'message:load',
+          'message:start',
+          'message:end',
+          'message:error',
+        ].forEach((event) => {
+          socket.on(event, (message) => forwardEvent(event, message))
+        })
+      })
+    },
+    forwardEvent,
+  }
 })()
