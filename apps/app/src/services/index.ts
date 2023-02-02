08a610def406349c360b3f8e0cc382bdf836d0cc
Wuriko <noahfenghom@gmail.com>
2023-02-02 20:38:51 +0100

    fix: improve incorrect api url paths

--- apps/app/src/services/index.ts
+++ apps/app/src/services/index.ts
@@ -3,11 +3,12 @@ import { io } from 'socket.io-client'
 import { useSettingsStore } from '@/features/settings/store'
 import { decrypt } from '@/utils/security'
 import { useSpeechStore } from '@/features/speech/store'
+import path from 'path-browserify'
 
-export const socket = io(`ws://localhost:${process.env.VUE_APP_SERVER_WS_PORT}`, {})
+export const socket = io(`ws://localhost:${ process.env.VUE_APP_SERVER_WS_PORT }`, {})
 
 export const localApi = axios.create({
-  baseURL: `http://localhost:${process.env.VUE_APP_SERVER_PORT}/api`,
+  baseURL: `http://localhost:${ process.env.VUE_APP_SERVER_PORT }/api`,
 })
 export const remoteApi = axios.create()
 export const api = (type?: 'remote' | 'local') => {
@@ -15,7 +16,7 @@ export const api = (type?: 'remote' | 'local') => {
   const speechStore = useSpeechStore()
   if (type === 'local') return localApi
   if (speechStore.hasUniversalApiCredentials) {
-    remoteApi.defaults.baseURL = `${settingsStore.universalApiEndpoint}/api`
+    remoteApi.defaults.baseURL = path.join(settingsStore.universalApiEndpoint, '/api')
     remoteApi.interceptors.request.use((config) => ({
       ...config,
       params: {
