ded07a68f644bcec38bb1d177612c8422d1e7137
Wurielle <Wurielle@users.noreply.github.com>
2023-02-02 22:38:46 +0000

    style: prettier formatting

--- apps/app/src/modules/electron-si/index.ts
+++ apps/app/src/modules/electron-si/index.ts
@@ -2,16 +2,17 @@ import { app } from 'electron'
 import nodeAbi from 'node-abi'
 
 export const ElectronSI = () => ({
-  getInfo: () => Promise.resolve({
-    version: app.getVersion(),
-    os: process.platform,
-    osVersion: process.getSystemVersion(),
-    arch: process.arch,
-    nodeVersion: process.versions.node,
-    electronVersion: process.versions.electron,
-    electronAbi: nodeAbi.getAbi(process.versions.electron, 'electron'),
-    chromiumVersion: process.versions.chrome,
-  }),
+  getInfo: () =>
+    Promise.resolve({
+      version: app.getVersion(),
+      os: process.platform,
+      osVersion: process.getSystemVersion(),
+      arch: process.arch,
+      nodeVersion: process.versions.node,
+      electronVersion: process.versions.electron,
+      electronAbi: nodeAbi.getAbi(process.versions.electron, 'electron'),
+      chromiumVersion: process.versions.chrome,
+    }),
 })
 
 export default ElectronSI()
--- apps/app/src/teams/messenger/views/NvSettingsSupport.vue
+++ apps/app/src/teams/messenger/views/NvSettingsSupport.vue
@@ -6,20 +6,16 @@
           <NvText type="subtitle">Thank you for using the software!</NvText>
           <NvText>
             Izabela has been developed on my own free time over countless nights, weekends and even
-            holidays for the
-            past {{ years }}
-            years.. It's been a passion project for so long and hearing
-            about how it
-            affected
+            holidays for the past {{ years }}
+            years.. It's been a passion project for so long and hearing about how it affected
             people's lives has been incredibly motivating!
           </NvText>
           <NvText>
-            This is my gift to the world and I
-            believe you'll make good use of it in turn! 🖤
+            This is my gift to the world and I believe you'll make good use of it in turn! 🖤
           </NvText>
           <NvText>
-            And please, if you enjoy the project, consider supporting me on <a
-            href="https://ko-fi.com/woowee" target="_blank">Ko-fi</a> to help me maintain it! Any
+            And please, if you enjoy the project, consider supporting me on
+            <a href="https://ko-fi.com/woowee" target="_blank">Ko-fi</a> to help me maintain it! Any
             help is greatly appreciated!
           </NvText>
         </NvStack>
@@ -30,5 +26,5 @@
 <script lang="ts" setup>
 import { NvCard, NvStack, NvText } from '@packages/ui'
 
-const years = new Date().getFullYear()-2017 // wow this long?
+const years = new Date().getFullYear() - 2017 // wow this long?
 </script>
