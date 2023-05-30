5b7ed02148917f70e8c9d7f5c7d1e6de3e0827f7
Wurielle <noahfenghom@gmail.com>
2023-05-30 12:29:29 +0200

    feat: lock app instance

--- apps/app/src/electron/app.ts
+++ apps/app/src/electron/app.ts
@@ -19,6 +19,8 @@ import registerElectronCache from '@/modules/electron-cache/register'
 import { destroyWinMouse } from '@/modules/node-mouse'
 
 const App = () => {
+  const gotTheLock = app.requestSingleInstanceLock()
+  if (!gotTheLock) app.quit()
   const isDevelopment = process.env.NODE_ENV !== 'production'
   const createWindows = () =>
     app
