0b627c1a4f74208aac8f950c7bcb9b185082bc71
Wurielle <Wurielle@users.noreply.github.com>
2024-09-08 16:15:45 +0000

    style: prettier formatting

--- packages/ui/src/components/overlay/Popover/popover.shared.ts
+++ packages/ui/src/components/overlay/Popover/popover.shared.ts
@@ -2,13 +2,13 @@ import { ExtractPropTypes, PropType } from 'vue'
 import { TippyOptions } from 'vue-tippy'
 
 export const sizeValues = ['sm', 'md', 'lg'] as const
-export type Size = typeof sizeValues[number]
+export type Size = (typeof sizeValues)[number]
 
 export const props = {
-    size: {
-        type: String as PropType<Size>,
-        default: 'md',
-    },
+  size: {
+    type: String as PropType<Size>,
+    default: 'md',
+  },
 }
 
 export type Props = ExtractPropTypes<typeof props>
