8d39d04e6762f084b8abfdb58d1df4918c645efa
Wuriko <noahfenghom@gmail.com>
2024-09-15 10:25:23 +0200

    fix: fix google cloud journey voices

--- apps/app-server/src/plugins/speech-apis/gc.ts
+++ apps/app-server/src/plugins/speech-apis/gc.ts
@@ -7,68 +7,68 @@ import fs from 'fs'
 import util from 'util'
 
 const plugin: Izabela.Server.Plugin = ({ app, config }) => {
-  const listVoicesHandler: RequestHandler = async (
-    {
-      body: {
-        credentials: { apiKey },
-      },
-    },
-    res,
-  ) => {
-    try {
-      const {
-        data: { voices },
-      } = await axios.get(
-        `https://texttospeech.googleapis.com/v1beta1/voices?key=${apiKey}`,
-      )
-      res.status(200).json(voices)
-    } catch (e: any) {
-      handleError(res, 'Internal server error', e.message, 500)
+    const listVoicesHandler: RequestHandler = async (
+        {
+            body: {
+                credentials: { apiKey },
+            },
+        },
+        res,
+    ) => {
+        try {
+            const {
+                data: { voices },
+            } = await axios.get(
+                `https://texttospeech.googleapis.com/v1beta1/voices?key=${ apiKey }`,
+            )
+            res.status(200).json(voices)
+        } catch (e: any) {
+            handleError(res, 'Internal server error', e.message, 500)
+        }
     }
-  }
 
-  const synthesizeSpeechHandler: RequestHandler = async (
-    {
-      body: {
-        credentials: { apiKey },
-        payload,
-      },
-    },
-    res,
-  ) => {
-    const outputFile = path.join(config?.tempPath || '', uuid() + '.mp3')
-    try {
-      fs.mkdirSync(path.parse(outputFile).dir, { recursive: true })
-      fs.writeFileSync(outputFile, '')
-      const {
-        data: { audioContent },
-      } = await axios.post(
-        `https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=${apiKey}`,
-        payload,
-      )
-      const writeFile = util.promisify(fs.writeFile)
+    const synthesizeSpeechHandler: RequestHandler = async (
+        {
+            body: {
+                credentials: { apiKey },
+                payload,
+            },
+        },
+        res,
+    ) => {
+        const outputFile = path.join(config?.tempPath || '', uuid()+'.wav')
+        try {
+            fs.mkdirSync(path.parse(outputFile).dir, { recursive: true })
+            fs.writeFileSync(outputFile, '')
+            const {
+                data: { audioContent },
+            } = await axios.post(
+                `https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=${ apiKey }`,
+                payload,
+            )
+            const writeFile = util.promisify(fs.writeFile)
 
-      await writeFile(outputFile, Buffer.from(audioContent, 'base64'), 'binary')
-      const stat = fs.statSync(outputFile)
-      const total = stat.size
+            await writeFile(outputFile, Buffer.from(audioContent, 'base64'), 'binary')
+            const stat = fs.statSync(outputFile)
+            const total = stat.size
 
-      res.writeHead(200, {
-        'Content-Length': total,
-        'Content-Type': 'audio/mp3',
-      })
-      const stream = fs.createReadStream(outputFile).pipe(res)
-      stream.on('finish', () => {
-        fs.unlinkSync(outputFile)
-      })
-    } catch (e: any) {
-      if (fs.existsSync(outputFile)) {
-        fs.unlinkSync(outputFile)
-      }
-      handleError(res, 'Internal server error', e.message, 500)
+            res.writeHead(200, {
+                'Content-Length': total,
+                'Content-Type': 'audio/wav',
+            })
+            const stream = fs.createReadStream(outputFile).pipe(res)
+            stream.on('finish', () => {
+                fs.unlinkSync(outputFile)
+            })
+        } catch (e: any) {
+            if (fs.existsSync(outputFile)) {
+                fs.unlinkSync(outputFile)
+            }
+            handleError(res, 'Internal server error', e.message, 500)
+        }
     }
-  }
-  app.post('/api/tts/google-cloud/list-voices', listVoicesHandler)
-  app.post('/api/tts/google-cloud/synthesize-speech', synthesizeSpeechHandler)
+    app.post('/api/tts/google-cloud/list-voices', listVoicesHandler)
+    app.post('/api/tts/google-cloud/synthesize-speech', synthesizeSpeechHandler)
 }
 
 export default plugin
--- apps/app/src/plugins/speech-engines/google-cloud/register.ts
+++ apps/app/src/plugins/speech-engines/google-cloud/register.ts
@@ -8,60 +8,60 @@ import { ENGINE_ID, ENGINE_NAME, getVoiceName } from './shared'
 import { getProperty, setProperty } from './store'
 
 const getCredentials = () => ({
-  apiKey: getProperty('apiKey', true),
+    apiKey: getProperty('apiKey', true),
 })
 
 const getSelectedVoice = () => getProperty('selectedVoice')
 registerEngine({
-  id: ENGINE_ID,
-  name: ENGINE_NAME,
-  category: 'cloud',
-  getSelectedVoice,
-  getVoiceName,
-  getCredentials,
-  hasCredentials() {
-    const speechStore = useSpeechStore()
-    return (
-      speechStore.hasUniversalApiCredentials ||
-      Object.values(getCredentials()).every(Boolean)
-    )
-  },
-  getPayload({ text, translatedText, voice: v }) {
-    const selectedVoice = getSelectedVoice()
-    const voice: any = pick(v || selectedVoice, [
-      'name',
-      'ssmlGender',
-      'languageCode',
-    ])
-    // eslint-disable-next-line prefer-destructuring
-    voice.languageCode = selectedVoice.languageCodes[0]
-    return {
-      input: {
-        text: translatedText || text,
-      },
-      voice,
-      audioConfig: {
-        audioEncoding: 'MP3',
-        volumeGainDb: 0,
-      },
-    }
-  },
-  getLanguageCode(voice) {
-    return (voice || getSelectedVoice()).languageCodes[0]
-  },
-  synthesizeSpeech({ credentials, payload }) {
-    return api(
-      getProperty('useLocalCredentials') ? 'local' : 'remote',
-    ).post<Blob>(
-      '/tts/google-cloud/synthesize-speech',
-      {
-        credentials,
-        payload,
-      },
-      { responseType: 'blob' },
-    )
-  },
-  voiceSelectComponent: NvVoiceSelect,
-  settingsComponent: NvSettings,
-  store: { setProperty, getProperty },
+    id: ENGINE_ID,
+    name: ENGINE_NAME,
+    category: 'cloud',
+    getSelectedVoice,
+    getVoiceName,
+    getCredentials,
+    hasCredentials() {
+        const speechStore = useSpeechStore()
+        return (
+            speechStore.hasUniversalApiCredentials ||
+            Object.values(getCredentials()).every(Boolean)
+        )
+    },
+    getPayload({ text, translatedText, voice: v }) {
+        const selectedVoice = getSelectedVoice()
+        const voice: any = pick(v || selectedVoice, [
+            'name',
+            'ssmlGender',
+            'languageCode',
+        ])
+        // eslint-disable-next-line prefer-destructuring
+        voice.languageCode = selectedVoice.languageCodes[0]
+        return {
+            input: {
+                text: translatedText || text,
+            },
+            voice,
+            audioConfig: {
+                audioEncoding: 'LINEAR16',
+                volumeGainDb: 0,
+            },
+        }
+    },
+    getLanguageCode(voice) {
+        return (voice || getSelectedVoice()).languageCodes[0]
+    },
+    synthesizeSpeech({ credentials, payload }) {
+        return api(
+            getProperty('useLocalCredentials') ? 'local' : 'remote',
+        ).post<Blob>(
+            '/tts/google-cloud/synthesize-speech',
+            {
+                credentials,
+                payload,
+            },
+            { responseType: 'blob' },
+        )
+    },
+    voiceSelectComponent: NvVoiceSelect,
+    settingsComponent: NvSettings,
+    store: { setProperty, getProperty },
 })
