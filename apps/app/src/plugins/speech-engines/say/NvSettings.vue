4a9f4fb893bef29d9f9dbcfc0449d2f115139903
Wurielle <Wurielle@users.noreply.github.com>
2024-10-03 20:17:09 +0000

    style: prettier formatting

--- apps/app/src/plugins/speech-engines/say/NvSettings.vue
+++ apps/app/src/plugins/speech-engines/say/NvSettings.vue
@@ -1,32 +1,38 @@
 <template>
   <NvFormItem label="Voice">
-    <NvVoiceSelect/>
+    <NvVoiceSelect />
   </NvFormItem>
-  <NvDivider direction="horizontal"/>
+  <NvDivider direction="horizontal" />
   <NvFormItem label="Speed">
     <NvGroup>
       <NvRangeInput
-          :max="300"
-          :min="1"
-          class="!grow"
-          v-bind="{
-              modelValue: getProperty('speed'),
-              'onUpdate:modelValue': (value) => setProperty('speed', value),
-            }"
+        :max="300"
+        :min="1"
+        class="!grow"
+        v-bind="{
+          modelValue: getProperty('speed'),
+          'onUpdate:modelValue': (value) => setProperty('speed', value),
+        }"
       />
       <NvNumberInput
-          :max="300"
-          :min="1"
-          v-bind="{
-              modelValue: getProperty('speed'),
-              'onUpdate:modelValue': (value) => setProperty('speed', value),
-            }"
+        :max="300"
+        :min="1"
+        v-bind="{
+          modelValue: getProperty('speed'),
+          'onUpdate:modelValue': (value) => setProperty('speed', value),
+        }"
       />
     </NvGroup>
   </NvFormItem>
 </template>
 <script lang="ts" setup>
-import { NvDivider, NvFormItem, NvGroup, NvNumberInput, NvRangeInput } from '@packages/ui'
+import {
+  NvDivider,
+  NvFormItem,
+  NvGroup,
+  NvNumberInput,
+  NvRangeInput,
+} from '@packages/ui'
 import NvVoiceSelect from './NvVoiceSelect'
 import { getProperty, setProperty } from '@/plugins/speech-engines/say/store.ts'
 </script>
