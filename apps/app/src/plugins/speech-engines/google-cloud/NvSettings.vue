e35d646a725372adb68ecf807f5dfb99893e5d95
Wuriko <noahfenghom@gmail.com>
2024-10-04 09:40:20 +0200

    feat: add google cloud options

--- apps/app/src/plugins/speech-engines/google-cloud/NvSettings.vue
+++ apps/app/src/plugins/speech-engines/google-cloud/NvSettings.vue
@@ -1,36 +1,110 @@
 <template>
   <NvAccessBlocker
-    :allowed="
+      :allowed="
       (speechStore.hasUniversalApiCredentials && !getProperty('useLocalCredentials')) ||
       !!getProperty('apiKey', true)
     "
-    reason="Credentials required"
+      reason="Credentials required"
   >
-    <NvFormItem label="Voice">
-      <NvVoiceSelect />
-    </NvFormItem>
+    <NvStack spacing="5">
+      <NvFormItem label="Voice">
+        <NvVoiceSelect/>
+      </NvFormItem>
+      <NvDivider direction="horizontal"/>
+      <NvFormItem label="Speaking Rate">
+        <NvGroup>
+          <NvRangeInput
+              :max="4"
+              :min="0.25"
+              :step=".01"
+              class="!grow"
+              v-bind="{
+                modelValue: getProperty('speakingRate'),
+                'onUpdate:modelValue': (value) => setProperty('speakingRate', value),
+              }"
+          />
+          <NvNumberInput
+              :max="4"
+              :min="0.25"
+              :step=".01"
+              v-bind="{
+                modelValue: getProperty('speakingRate'),
+                'onUpdate:modelValue': (value) => setProperty('speakingRate', value),
+              }"
+          />
+        </NvGroup>
+      </NvFormItem>
+      <NvDivider direction="horizontal"/>
+      <NvFormItem label="Pitch">
+        <NvGroup>
+          <NvRangeInput
+              :max="20"
+              :min="-20"
+              :step=".1"
+              class="!grow"
+              v-bind="{
+                modelValue: getProperty('pitch'),
+                'onUpdate:modelValue': (value) => setProperty('pitch', value),
+              }"
+          />
+          <NvNumberInput
+              :max="20"
+              :min="-20"
+              :step=".1"
+              v-bind="{
+                modelValue: getProperty('pitch'),
+                'onUpdate:modelValue': (value) => setProperty('pitch', value),
+              }"
+          />
+        </NvGroup>
+      </NvFormItem>
+      <NvDivider direction="horizontal"/>
+      <NvFormItem label="Volume Gain (Db)">
+        <NvGroup>
+          <NvRangeInput
+              :max="16"
+              :min="-96"
+              :step=".1"
+              class="!grow"
+              v-bind="{
+                modelValue: getProperty('volumeGainDb'),
+                'onUpdate:modelValue': (value) => setProperty('volumeGainDb', value),
+              }"
+          />
+          <NvNumberInput
+              :max="20"
+              :min="-20"
+              :step=".1"
+              v-bind="{
+                modelValue: getProperty('volumeGainDb'),
+                'onUpdate:modelValue': (value) => setProperty('volumeGainDb', value),
+              }"
+          />
+        </NvGroup>
+      </NvFormItem>
+    </NvStack>
   </NvAccessBlocker>
   <template v-if="speechStore.hasUniversalApiCredentials">
-    <NvDivider direction="horizontal" />
+    <NvDivider direction="horizontal"/>
     <NvGroup justify="apart" no-wrap spacing="5">
       <NvStack>
         <NvText type="label">Use my own credentials</NvText>
       </NvStack>
       <NvSwitch
-        :modelValue="getProperty('useLocalCredentials')"
-        @update:modelValue="(value) => setProperty('useLocalCredentials', value)"
+          :modelValue="getProperty('useLocalCredentials')"
+          @update:modelValue="(value) => setProperty('useLocalCredentials', value)"
       />
     </NvGroup>
   </template>
   <template v-if="getProperty('useLocalCredentials') || !speechStore.hasUniversalApiCredentials">
-    <NvDivider direction="horizontal" />
+    <NvDivider direction="horizontal"/>
     <NvStack spacing="5">
       <NvFormItem label="API Key">
         <NvInput
-          :modelValue="getProperty('apiKey', true)"
-          show-password
-          type="password"
-          @update:modelValue="(value) => setProperty('apiKey', value, true)"
+            :modelValue="getProperty('apiKey', true)"
+            show-password
+            type="password"
+            @update:modelValue="(value) => setProperty('apiKey', value, true)"
         />
       </NvFormItem>
     </NvStack>
@@ -42,7 +116,7 @@ import {
   NvDivider,
   NvFormItem,
   NvGroup,
-  NvInput,
+  NvInput, NvNumberInput, NvRangeInput,
   NvStack,
   NvSwitch,
   NvText,
--- apps/app/src/plugins/speech-engines/google-cloud/store.ts
+++ apps/app/src/plugins/speech-engines/google-cloud/store.ts
@@ -2,12 +2,15 @@ import { definePluginStore } from '@/store'
 import { ENGINE_ID } from './shared'
 
 export const { setProperty, getProperty } = definePluginStore(ENGINE_ID, {
-  apiKey: '',
-  selectedVoice: {
-    languageCodes: ['en-GB'],
-    ssmlGender: 'FEMALE',
-    name: 'en-GB-Wavenet-A',
-  },
-  useLocalCredentials: false,
-  favoriteVoiceIds: [],
+    apiKey: '',
+    selectedVoice: {
+        languageCodes: ['en-GB'],
+        ssmlGender: 'FEMALE',
+        name: 'en-GB-Wavenet-A',
+    },
+    useLocalCredentials: false,
+    favoriteVoiceIds: [],
+    speakingRate: 1,
+    pitch: 0,
+    volumeGainDb: 0,
 })
