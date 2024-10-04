2965484c5eef13a8161c266b57a675d3152721c8
Wurielle <Wurielle@users.noreply.github.com>
2024-10-04 08:49:01 +0000

    style: prettier formatting

--- apps/app/src/plugins/speech-engines/google-cloud/NvSettings.vue
+++ apps/app/src/plugins/speech-engines/google-cloud/NvSettings.vue
@@ -1,110 +1,122 @@
 <template>
   <NvAccessBlocker
-      :allowed="
-      (speechStore.hasUniversalApiCredentials && !getProperty('useLocalCredentials')) ||
+    :allowed="
+      (speechStore.hasUniversalApiCredentials &&
+        !getProperty('useLocalCredentials')) ||
       !!getProperty('apiKey', true)
     "
-      reason="Credentials required"
+    reason="Credentials required"
   >
     <NvStack spacing="5">
       <NvFormItem label="Voice">
-        <NvVoiceSelect/>
+        <NvVoiceSelect />
       </NvFormItem>
-      <NvDivider direction="horizontal"/>
+      <NvDivider direction="horizontal" />
       <NvFormItem label="Speaking Rate">
         <NvGroup>
           <NvRangeInput
-              :max="4"
-              :min="0.25"
-              :step=".01"
-              class="!grow"
-              v-bind="{
-                modelValue: getProperty('speakingRate'),
-                'onUpdate:modelValue': (value) => setProperty('speakingRate', value),
-              }"
+            :max="4"
+            :min="0.25"
+            :step="0.01"
+            class="!grow"
+            v-bind="{
+              modelValue: getProperty('speakingRate'),
+              'onUpdate:modelValue': (value) =>
+                setProperty('speakingRate', value),
+            }"
           />
           <NvNumberInput
-              :max="4"
-              :min="0.25"
-              :step=".01"
-              v-bind="{
-                modelValue: getProperty('speakingRate'),
-                'onUpdate:modelValue': (value) => setProperty('speakingRate', value),
-              }"
+            :max="4"
+            :min="0.25"
+            :step="0.01"
+            v-bind="{
+              modelValue: getProperty('speakingRate'),
+              'onUpdate:modelValue': (value) =>
+                setProperty('speakingRate', value),
+            }"
           />
         </NvGroup>
       </NvFormItem>
-      <NvDivider direction="horizontal"/>
+      <NvDivider direction="horizontal" />
       <NvFormItem label="Pitch">
         <NvGroup>
           <NvRangeInput
-              :max="20"
-              :min="-20"
-              :step=".1"
-              class="!grow"
-              v-bind="{
-                modelValue: getProperty('pitch'),
-                'onUpdate:modelValue': (value) => setProperty('pitch', value),
-              }"
+            :max="20"
+            :min="-20"
+            :step="0.1"
+            class="!grow"
+            v-bind="{
+              modelValue: getProperty('pitch'),
+              'onUpdate:modelValue': (value) => setProperty('pitch', value),
+            }"
           />
           <NvNumberInput
-              :max="20"
-              :min="-20"
-              :step=".1"
-              v-bind="{
-                modelValue: getProperty('pitch'),
-                'onUpdate:modelValue': (value) => setProperty('pitch', value),
-              }"
+            :max="20"
+            :min="-20"
+            :step="0.1"
+            v-bind="{
+              modelValue: getProperty('pitch'),
+              'onUpdate:modelValue': (value) => setProperty('pitch', value),
+            }"
           />
         </NvGroup>
       </NvFormItem>
-      <NvDivider direction="horizontal"/>
+      <NvDivider direction="horizontal" />
       <NvFormItem label="Volume Gain (Db)">
         <NvGroup>
           <NvRangeInput
-              :max="16"
-              :min="-96"
-              :step=".1"
-              class="!grow"
-              v-bind="{
-                modelValue: getProperty('volumeGainDb'),
-                'onUpdate:modelValue': (value) => setProperty('volumeGainDb', value),
-              }"
+            :max="16"
+            :min="-96"
+            :step="0.1"
+            class="!grow"
+            v-bind="{
+              modelValue: getProperty('volumeGainDb'),
+              'onUpdate:modelValue': (value) =>
+                setProperty('volumeGainDb', value),
+            }"
           />
           <NvNumberInput
-              :max="20"
-              :min="-20"
-              :step=".1"
-              v-bind="{
-                modelValue: getProperty('volumeGainDb'),
-                'onUpdate:modelValue': (value) => setProperty('volumeGainDb', value),
-              }"
+            :max="20"
+            :min="-20"
+            :step="0.1"
+            v-bind="{
+              modelValue: getProperty('volumeGainDb'),
+              'onUpdate:modelValue': (value) =>
+                setProperty('volumeGainDb', value),
+            }"
           />
         </NvGroup>
       </NvFormItem>
     </NvStack>
   </NvAccessBlocker>
   <template v-if="speechStore.hasUniversalApiCredentials">
-    <NvDivider direction="horizontal"/>
+    <NvDivider direction="horizontal" />
     <NvGroup justify="apart" no-wrap spacing="5">
       <NvStack>
         <NvText type="label">Use my own credentials</NvText>
       </NvStack>
       <NvSwitch
-          :modelValue="getProperty('useLocalCredentials')"
-          @update:modelValue="(value) => setProperty('useLocalCredentials', value)"
+        :modelValue="getProperty('useLocalCredentials')"
+        @update:modelValue="
+          (value) => setProperty('useLocalCredentials', value)
+        "
       />
     </NvGroup>
   </template>
-  <template v-if="getProperty('useLocalCredentials') || !speechStore.hasUniversalApiCredentials">
-    <NvDivider direction="horizontal"/>
+  <template
+    v-if="
+      getProperty('useLocalCredentials') ||
+      !speechStore.hasUniversalApiCredentials
+    "
+  >
+    <NvDivider direction="horizontal" />
     <NvStack spacing="5">
       <NvFormItem label="API Key">
         <NvInput
-            :modelValue="getProperty('apiKey', true)"
-            show-password
-            type="password"
-            @update:modelValue="(value) => setProperty('apiKey', value, true)"
+          :modelValue="getProperty('apiKey', true)"
+          show-password
+          type="password"
+          @update:modelValue="(value) => setProperty('apiKey', value, true)"
         />
       </NvFormItem>
     </NvStack>
@@ -116,7 +128,9 @@ import {
   NvDivider,
   NvFormItem,
   NvGroup,
-  NvInput, NvNumberInput, NvRangeInput,
+  NvInput,
+  NvNumberInput,
+  NvRangeInput,
   NvStack,
   NvSwitch,
   NvText,
