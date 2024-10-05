6c4a86e9e6a1f3dacc57510fdb69bc2281c64e40
Wuriko <noahfenghom@gmail.com>
2024-10-05 11:14:21 +0200

    ci: fix electron publish

--- apps/app/electron-builder.config.js
+++ apps/app/electron-builder.config.js
@@ -6,11 +6,11 @@ const fs = require('fs')
 const pkg = require('./package.json')
 
 function getElectronVersion() {
-  const electronPath = require.resolve('electron')
+    const electronPath = require.resolve('electron')
 
-  const data = fs.readFileSync(path.join(electronPath, '..', 'package.json'))
-  const version = JSON.parse(data.toString())?.version
-  return version
+    const data = fs.readFileSync(path.join(electronPath, '..', 'package.json'))
+    const version = JSON.parse(data.toString())?.version
+    return version
 }
 
 /**
@@ -18,25 +18,26 @@ function getElectronVersion() {
  * @see https://www.electron.build/configuration/configuration
  */
 module.exports = {
-  $schema:
-    'https://raw.githubusercontent.com/electron-userland/electron-builder/master/packages/app-builder-lib/scheme.json',
-  asar: true,
-  productName: pkg.productName,
-  directories: {
-    output: 'dist_electron',
-  },
-  files: ['dist', 'dist-electron'],
-  nsis: {
-    oneClick: true,
-    perMachine: false,
-    allowToChangeInstallationDirectory: false,
-    deleteAppDataOnUninstall: false,
-  },
-  appId: 'com.nhs.izabela',
-  generateUpdatesFilesForAllChannels: true,
-  // eslint-disable-next-line no-template-curly-in-string
-  artifactName: '${name}-setup-${version}-${os}.${ext}',
-  publish: ['github'],
-  electronVersion: getElectronVersion(),
-  extraFiles: ['./resources/**'],
+    $schema:
+        'https://raw.githubusercontent.com/electron-userland/electron-builder/master/packages/app-builder-lib/scheme.json',
+    asar: true,
+    productName: pkg.productName,
+    directories: {
+        output: 'dist_electron',
+    },
+    files: ['dist', 'dist-electron'],
+    nsis: {
+        oneClick: true,
+        perMachine: false,
+        allowToChangeInstallationDirectory: false,
+        deleteAppDataOnUninstall: false,
+    },
+    appId: 'com.nhs.izabela',
+    generateUpdatesFilesForAllChannels: true,
+    // eslint-disable-next-line no-template-curly-in-string
+    artifactName: '${name}-setup-${version}-${os}.${ext}',
+    publish: ['github'],
+    publishAutoUpdate: false,
+    electronVersion: getElectronVersion(),
+    extraFiles: ['./resources/**'],
 }
