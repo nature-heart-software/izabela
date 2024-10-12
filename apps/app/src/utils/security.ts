96814b86453cad2b5b35d5a827dd2692297dd129
Wuriko <noahfenghom@gmail.com>
2024-10-12 23:25:39 +0200

    fix: fix decrypt error

--- apps/app/src/utils/security.ts
+++ apps/app/src/utils/security.ts
@@ -1,20 +1,26 @@
 import CryptoJS from 'crypto-js'
 
 export const encrypt = (valueToEncrypt: string) =>
-  CryptoJS.AES.encrypt(
-    valueToEncrypt,
-    import.meta.env.VITE_ENCRYPTION_KEY as string,
-  ).toString()
+    CryptoJS.AES.encrypt(
+        valueToEncrypt,
+        import.meta.env.VITE_APP_ENCRYPTION_KEY as string,
+    ).toString()
 
 export const decrypt = (valueToDecrypt: string) => {
-  const bytes = CryptoJS.AES.decrypt(
-    valueToDecrypt,
-    import.meta.env.VITE_ENCRYPTION_KEY || '',
-  )
-  return bytes.toString(CryptoJS.enc.Utf8)
+    let result = ''
+    try {
+        const bytes = CryptoJS.AES.decrypt(
+            valueToDecrypt,
+            import.meta.env.VITE_APP_ENCRYPTION_KEY || '',
+        )
+        bytes.toString(CryptoJS.enc.Utf8)
+    } catch (e) {
+        console.warn("Could not decrypt value.")
+    }
+    return result
 }
 
 export const useEncryption = () => ({
-  encrypt,
-  decrypt,
+    encrypt,
+    decrypt,
 })
