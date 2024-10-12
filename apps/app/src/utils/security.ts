0bd809166f1751531d44bbf8e1308a283ae18759
Wurielle <Wurielle@users.noreply.github.com>
2024-10-12 22:02:05 +0000

    style: prettier formatting

--- apps/app/src/utils/security.ts
+++ apps/app/src/utils/security.ts
@@ -1,26 +1,26 @@
 import CryptoJS from 'crypto-js'
 
 export const encrypt = (valueToEncrypt: string) =>
-    CryptoJS.AES.encrypt(
-        valueToEncrypt,
-        import.meta.env.VITE_APP_ENCRYPTION_KEY as string,
-    ).toString()
+  CryptoJS.AES.encrypt(
+    valueToEncrypt,
+    import.meta.env.VITE_APP_ENCRYPTION_KEY as string,
+  ).toString()
 
 export const decrypt = (valueToDecrypt: string) => {
-    let result = ''
-    try {
-        const bytes = CryptoJS.AES.decrypt(
-            valueToDecrypt,
-            import.meta.env.VITE_APP_ENCRYPTION_KEY || '',
-        )
-        result = bytes.toString(CryptoJS.enc.Utf8)
-    } catch (e) {
-        console.warn('Could not decrypt value.')
-    }
-    return result
+  let result = ''
+  try {
+    const bytes = CryptoJS.AES.decrypt(
+      valueToDecrypt,
+      import.meta.env.VITE_APP_ENCRYPTION_KEY || '',
+    )
+    result = bytes.toString(CryptoJS.enc.Utf8)
+  } catch (e) {
+    console.warn('Could not decrypt value.')
+  }
+  return result
 }
 
 export const useEncryption = () => ({
-    encrypt,
-    decrypt,
+  encrypt,
+  decrypt,
 })
