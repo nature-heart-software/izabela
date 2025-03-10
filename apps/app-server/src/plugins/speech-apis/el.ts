f802468eb5a34c32bf5fd75ab5ccf28ebd403b63
Wurielle <Wurielle@users.noreply.github.com>
2025-03-10 10:18:33 +0000

    style: prettier formatting

--- apps/app-server/src/plugins/speech-apis/el.ts
+++ apps/app-server/src/plugins/speech-apis/el.ts
@@ -1,87 +1,86 @@
 import { RequestHandler } from 'express'
 import axios from 'axios'
 import { handleError } from '../../utils/requests'
-import { ElevenLabsClient } from "elevenlabs"
+import { ElevenLabsClient } from 'elevenlabs'
 
 const plugin: Izabela.Server.Plugin = ({ app }) => {
-    const api = axios.create({
-        baseURL: 'https://api.elevenlabs.io/v1',
-    })
-    const listVoicesHandler: RequestHandler = async (
-        {
-            body: {
-                credentials: { apiKey },
-            },
-        },
-        res,
-    ) => {
-        try {
-            const client = new ElevenLabsClient({ apiKey })
-            const { voices } = await client.voices.getAll({
-                show_legacy: true,
-            })
-            res.status(200).json(voices)
-        } catch (e: any) {
-            handleError(res, 'Internal server error', e.message, 500)
-        }
+  const api = axios.create({
+    baseURL: 'https://api.elevenlabs.io/v1',
+  })
+  const listVoicesHandler: RequestHandler = async (
+    {
+      body: {
+        credentials: { apiKey },
+      },
+    },
+    res,
+  ) => {
+    try {
+      const client = new ElevenLabsClient({ apiKey })
+      const { voices } = await client.voices.getAll({
+        show_legacy: true,
+      })
+      res.status(200).json(voices)
+    } catch (e: any) {
+      handleError(res, 'Internal server error', e.message, 500)
     }
+  }
 
-    const listModelsHandler: RequestHandler = async (
-        {
-            body: {
-                credentials: { apiKey },
-            },
-        },
-        res,
-    ) => {
-        try {
-            const client = new ElevenLabsClient({ apiKey })
-            const models = await client.models.getAll()
-            res.status(200).json(models)
-        } catch (e: any) {
-            handleError(res, 'Internal server error', e.message, 500)
-        }
+  const listModelsHandler: RequestHandler = async (
+    {
+      body: {
+        credentials: { apiKey },
+      },
+    },
+    res,
+  ) => {
+    try {
+      const client = new ElevenLabsClient({ apiKey })
+      const models = await client.models.getAll()
+      res.status(200).json(models)
+    } catch (e: any) {
+      handleError(res, 'Internal server error', e.message, 500)
     }
+  }
 
-    const synthesizeSpeechHandler: RequestHandler = async (
-        {
-            body: {
-                credentials: { apiKey },
-                payload: {
-                    text,
-                    voice,
-                    stability,
-                    similarity_boost,
-                    use_speaker_boost,
-                    style,
-                    model_id,
-                },
-            },
+  const synthesizeSpeechHandler: RequestHandler = async (
+    {
+      body: {
+        credentials: { apiKey },
+        payload: {
+          text,
+          voice,
+          stability,
+          similarity_boost,
+          use_speaker_boost,
+          style,
+          model_id,
         },
-        res,
-    ) => {
-        try {
-            const client = new ElevenLabsClient({ apiKey })
-            const stream = await client.textToSpeech.convertAsStream(voice.voice_id, {
-                text,
-                model_id,
-                voice_settings: {
-                    stability,
-                    similarity_boost,
-                    use_speaker_boost,
-                    style,
-                },
-            })
-            stream.pipe(res)
-            stream.on('finish', () => {
-            })
-        } catch (e: any) {
-            handleError(res, 'Internal server error', e.message, 500)
-        }
+      },
+    },
+    res,
+  ) => {
+    try {
+      const client = new ElevenLabsClient({ apiKey })
+      const stream = await client.textToSpeech.convertAsStream(voice.voice_id, {
+        text,
+        model_id,
+        voice_settings: {
+          stability,
+          similarity_boost,
+          use_speaker_boost,
+          style,
+        },
+      })
+      stream.pipe(res)
+      stream.on('finish', () => {})
+    } catch (e: any) {
+      handleError(res, 'Internal server error', e.message, 500)
     }
-    app.post('/api/tts/elevenlabs/list-voices', listVoicesHandler)
-    app.post('/api/tts/elevenlabs/list-models', listModelsHandler)
-    app.post('/api/tts/elevenlabs/synthesize-speech', synthesizeSpeechHandler)
+  }
+  app.post('/api/tts/elevenlabs/list-voices', listVoicesHandler)
+  app.post('/api/tts/elevenlabs/list-models', listModelsHandler)
+  app.post('/api/tts/elevenlabs/synthesize-speech', synthesizeSpeechHandler)
 }
 
 export default plugin
