daa990de115b644af4ef3107ef0f90135972c236
Wurielle <Wurielle@users.noreply.github.com>
2025-03-11 07:36:24 +0000

    style: prettier formatting

--- apps/app/src/plugins/speech-engines/sam/store.ts
+++ apps/app/src/plugins/speech-engines/sam/store.ts
@@ -2,12 +2,12 @@ import { definePluginStore } from '@/store'
 import { defaultVoice, ENGINE_ID } from './shared'
 
 export const { setProperty, getProperty } = definePluginStore(ENGINE_ID, {
-    selectedVoice: {
-        ...defaultVoice,
-    },
-    speed: 72,
-    pitch: 64,
-    throat: 128,
-    mouth: 128,
-    favoriteVoiceIds: [],
+  selectedVoice: {
+    ...defaultVoice,
+  },
+  speed: 72,
+  pitch: 64,
+  throat: 128,
+  mouth: 128,
+  favoriteVoiceIds: [],
 })
