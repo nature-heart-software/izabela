c5e823bf30d4a37b7fcfb20bf446086932378f45
Wuriko <noahfenghom@gmail.com>
2025-03-16 11:05:52 +0100

    fix: fix in-game overlay size

--- apps/app/src/electron/renderer.ts
+++ apps/app/src/electron/renderer.ts
@@ -3,7 +3,7 @@ import '@/modules/electron-log/renderer'
 import { onIPCGameOverlayResize } from '@/electron/events/renderer.ts'
 
 onIPCGameOverlayResize(({ width, height }) => {
-  const appEl = document.getElementById('app')
-  appEl.style.width = width+'px'
-  appEl.style.height = height+'px'
+    const appEl = document.getElementById('app')
+    appEl.style.width = width+'px'
+    appEl.style.height = height+'px'
 })
--- apps/app/src/teams/messenger/electron/overlay-window.ts
+++ apps/app/src/teams/messenger/electron/overlay-window.ts
@@ -1,57 +1,85 @@
-import { app, BrowserWindow } from 'electron'
+import { app, screen, BrowserWindow } from 'electron'
 import path from 'path'
 import { createProtocol } from '@/electron/utils'
 import { ipcMain } from 'electron-postman'
 import gameOverlay from '@/electron/game-overlay'
 
+const getLargestMonitorSize = () => {
+    const displays = screen.getAllDisplays()
+    let width = 0
+    let height = 0
+
+    displays.forEach(display => {
+        const { size } = display
+        if (size.width > width) width = size.width
+        if (size.height > height) height = size.height
+    })
+
+    return { width, height }
+}
+
 let window: BrowserWindow
 const createWindow = async (name: string): Promise<BrowserWindow> => {
-  const overlayDebug = import.meta.env.DEV ? false : false
+    const overlayDebug = import.meta.env.DEV ? false : false
 
-  window = gameOverlay.createWindow(name, {
-    x: 0,
-    y: 0,
-    height: 1080,
-    width: 1920,
-    frame: false,
-    show: overlayDebug,
-    transparent: true,
-    resizable: false,
-    enableLargerThanScreen: true,
-    webPreferences: {
-      offscreen: !overlayDebug,
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: Boolean(
-        Number(import.meta.env.VITE_ELECTRON_NODE_INTEGRATION),
-      ),
-      contextIsolation: !Number(import.meta.env.VITE_ELECTRON_NODE_INTEGRATION),
-      sandbox: false,
-    },
-  })
-  window.webContents.setMaxListeners(Infinity)
-  window.setSize(1920, 1080)
-  window.webContents.setFrameRate(240)
-  ipcMain.registerBrowserWindow(name, window)
 
-  window.webContents.once('did-finish-load', () => {
-    if (import.meta.env.DEV) {
-      window.webContents.openDevTools({ mode: 'undocked' })
+    const { width, height } = getLargestMonitorSize()
+    console.log(width, height)
+    window = gameOverlay.createWindow(name, {
+        x: 0,
+        y: 0,
+        height,
+        width,
+        frame: false,
+        show: overlayDebug,
+        transparent: true,
+        resizable: false,
+        enableLargerThanScreen: true,
+        webPreferences: {
+            offscreen: !overlayDebug,
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: Boolean(
+                Number(import.meta.env.VITE_ELECTRON_NODE_INTEGRATION),
+            ),
+            contextIsolation: !Number(import.meta.env.VITE_ELECTRON_NODE_INTEGRATION),
+            sandbox: false,
+        },
+    })
+    window.webContents.setMaxListeners(Infinity)
+    window.setSize(width, height)
+    window.webContents.setFrameRate(240)
+    ipcMain.registerBrowserWindow(name, window)
+
+    screen.on('display-metrics-changed', () => {
+        const { width, height } = getLargestMonitorSize()
+        window.setBounds({
+            x: 0,
+            y: 0,
+            width,
+            height,
+        })
+        window.setSize(width, height)
+    })
+
+    window.webContents.once('did-finish-load', () => {
+        if (import.meta.env.DEV) {
+            window.webContents.openDevTools({ mode: 'undocked' })
+        }
+    })
+
+    const filePath = `./src/teams/messenger/index.html?game-overlay`
+    if (import.meta.env.VITE_DEV_SERVER_URL) {
+        await window.loadURL(path.join(import.meta.env.VITE_DEV_SERVER_URL as string, filePath))
+    } else {
+        createProtocol('app')
+        await window.loadURL(`app://${ filePath }`)
     }
-  })
 
-  const filePath = `./src/teams/messenger/index.html?game-overlay`
-  if (import.meta.env.VITE_DEV_SERVER_URL) {
-    await window.loadURL(path.join(import.meta.env.VITE_DEV_SERVER_URL as string, filePath))
-  } else {
-    createProtocol('app')
-    await window.loadURL(`app://${ filePath }`)
-  }
+    if (!overlayDebug) {
+        gameOverlay.isReady().then(() => gameOverlay.addOverlayWindow(name, window, 0, 0))
+    }
 
-  if (!overlayDebug) {
-    gameOverlay.isReady().then(() => gameOverlay.addOverlayWindow(name, window, 0, 0))
-  }
-
-  return window
+    return window
 }
 
 export default createWindow
