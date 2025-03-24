34ba977a209d9a02949ad8f4041a962a46279d7a
Wuriko <noahfenghom@gmail.com>
2025-03-24 21:20:38 +0100

    fix: fix shortcuts not working when running with admin rights

--- apps/app/src/electron/app.ts
+++ apps/app/src/electron/app.ts
@@ -8,8 +8,8 @@ import { createPinia } from 'pinia'
 import createTray from '@/teams/tray/electron-tray'
 import ElectronWindowManager from '@/modules/electron-window-manager'
 import {
-  createMessengerGameOverlayWindow,
-  createMessengerWindow,
+    createMessengerGameOverlayWindow,
+    createMessengerWindow,
 } from '@/teams/messenger/electron/background'
 import { createSpeechWorkerWindow } from '@/teams/speech-worker/electron/background'
 import { bridgeModules } from '@/electron/bridge'
@@ -26,180 +26,184 @@ import gameOverlay from '@/electron/game-overlay.ts'
 import { gkl } from '@/modules/electron-keybinding/utils.ts'
 
 const App = () => {
-  const isDevelopment = import.meta.env.DEV
-  const createWindows = () =>
-    app
-      .whenReady()
-      .then(async () =>
-        Promise.all([
-          ElectronWindowManager.registerInstance(
-            'messenger',
-            createMessengerWindow,
-          ),
-          ElectronWindowManager.registerInstance(
-            'overlay',
-            createOverlayWindow,
-          ),
-          ElectronWindowManager.registerInstance(
-            'speech-worker',
-            createSpeechWorkerWindow,
-          ),
-          ElectronWindowManager.registerInstance(
-            'messenger-game-overlay',
-            createMessengerGameOverlayWindow,
-          ),
-        ]),
-      )
+    const isDevelopment = import.meta.env.DEV
+    const createWindows = () =>
+        app
+            .whenReady()
+            .then(async () =>
+                Promise.all([
+                    ElectronWindowManager.registerInstance(
+                        'messenger',
+                        createMessengerWindow,
+                    ),
+                    ElectronWindowManager.registerInstance(
+                        'overlay',
+                        createOverlayWindow,
+                    ),
+                    ElectronWindowManager.registerInstance(
+                        'speech-worker',
+                        createSpeechWorkerWindow,
+                    ),
+                    ElectronWindowManager.registerInstance(
+                        'messenger-game-overlay',
+                        createMessengerGameOverlayWindow,
+                    ),
+                ]),
+            )
 
-  const registerElectronPinia = () => {
-    createApp(h({})).use(
-      createPinia().use((electronPiniaPlugin.default || electronPiniaPlugin)()),
-    )
-  }
-
-  const startGameOverlay = async () =>
-    app.whenReady().then(() => gameOverlay.start())
-
-  const startAppServer = async () =>
-    app.whenReady().then(async () =>
-      server.start({
-        tempPath: path.join(app.getPath('userData'), 'temp'),
-        port: import.meta.env.VITE_SERVER_PORT,
-        ws: {
-          port: import.meta.env.VITE_SERVER_WS_PORT,
-        },
-      }),
-    )
-
-  const configureAppDefaults = () => {
-    if (process.platform === 'win32') app.setAppUserModelId(app.name)
-    app.commandLine.appendSwitch('disable-renderer-backgrounding')
-    app.commandLine.appendSwitch('ignore-certificate-errors')
-    app.commandLine.appendSwitch('wm-window-animations-disabled')
-
-    /* Disabling Hardware Acceleration does the following:
-     * - fixes ui freeze in DevTools when unfocused
-     * - fixes ui freeze on other hardware accelerated softwares (chrome, vs code, ...)
-     * - fixes element selection in DevTools
-     */
-    app.disableHardwareAcceleration()
-
-    // Scheme must be registered before the app is ready
-    protocol.registerSchemesAsPrivileged([
-      { scheme: 'app', privileges: { secure: true, standard: true } },
-    ])
-  }
-
-  function exec(description: string, action: () => any) {
-    console.log(`[app]: ${description}`)
-    return action()
-  }
-
-  const onQuit = () => {
-    gkl?.kill()
-    gameOverlay.quit()
-    /* fixes app.quit(): https://stackoverflow.com/a/75369483 */
-    ElectronWindowManager.getInstances().forEach(({ window }) => {
-      window.removeAllListeners()
-    })
-    destroyWinMouse()
-  }
-
-  function addEventListeners() {
-    app.whenReady().then(() => {
-      const credentialsDirPath = path.join(
-        app.getPath('userData'),
-        'credentials',
-      )
-      const googleCloudSpeechCredentialsFilePath = path.join(
-        credentialsDirPath,
-        'google-cloud-speech-credentials.json',
-      )
-      process.env.GOOGLE_APPLICATION_CREDENTIALS =
-        googleCloudSpeechCredentialsFilePath
-    })
-
-    app.on('ready', async () => {
-      if (isDevelopment) {
-        try {
-          await ((installExtension as any).default as typeof installExtension)(
-            VUEJS_DEVTOOLS,
-          )
-        } catch (e) {
-          console.error(
-            'Vue Devtools failed to install:',
-            (e as Error).toString(),
-          )
-        }
-      }
-    })
-
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindows()
-    })
-
-    const handleQuit = (quitApp = false) => {
-      onQuit()
-      if (quitApp) {
-        app.quit()
-      }
+    const registerElectronPinia = () => {
+        createApp(h({})).use(
+            createPinia().use((electronPiniaPlugin.default || electronPiniaPlugin)()),
+        )
     }
 
-    process.on('message', (data) => {
-      if (process.platform === 'win32' && data === 'graceful-exit') {
-        handleQuit(true)
-      }
-    })
-    ;['SIGINT', 'SIGTERM', 'SIGQUIT'].forEach((signal) => {
-      process.on(signal, () => {
-        handleQuit(true)
-      })
-    })
+    const startGameOverlay = async () =>
+        app.whenReady().then(() => gameOverlay.start())
 
-    app.on('before-quit', () => {
-      handleQuit()
-    })
-
-    app.on('window-all-closed', () => {
-      if (process.platform !== 'darwin') {
-        handleQuit(true)
-      }
-    })
-
-    app.on('web-contents-created', (_, webContents) => {
-      webContents.on('preload-error', (_, preloadPath, error) => {
-        console.error(
-          `Preload script error:\nPath: ${preloadPath}\nError:`,
-          error,
+    const startAppServer = async () =>
+        app.whenReady().then(async () =>
+            server.start({
+                tempPath: path.join(app.getPath('userData'), 'temp'),
+                port: import.meta.env.VITE_SERVER_PORT,
+                ws: {
+                    port: import.meta.env.VITE_SERVER_WS_PORT,
+                },
+            }),
         )
-      })
-    })
-  }
 
-  function start() {
-    return Promise.all([
-      exec('Register app listeners', () => addEventListeners()),
-      exec('Configure app defaults', () => configureAppDefaults()),
-      exec('Register electron-pinia', () => registerElectronPinia()),
-      exec('Register updater', () => registerElectronUpdater()),
-      exec('Register startup', () => registerElectronStartup()),
-      exec('Register debug', () => registerElectronDebug()),
-      exec('Bridge modules', () => bridgeModules()),
-      exec('Start Game Overlay', () => startGameOverlay()),
-      exec('Create tray', () => createTray()),
-      exec('Create windows', () => createWindows()),
-      exec('Start server', () => startAppServer()),
-      exec('Register display', () => registerElectronDisplay()),
-      exec('Register keybindings', () => registerElectronKeybinding()),
-      exec('Handle Cache', () => registerElectronCache()),
-    ])
-  }
+    const configureAppDefaults = () => {
+        if (process.platform === 'win32') app.setAppUserModelId(app.name)
+        app.commandLine.appendSwitch('disable-renderer-backgrounding')
+        app.commandLine.appendSwitch('ignore-certificate-errors')
+        app.commandLine.appendSwitch('wm-window-animations-disabled')
 
-  return {
-    start,
-    isDevelopment,
-    exec,
-  }
+        /* Disabling Hardware Acceleration does the following:
+         * - fixes ui freeze in DevTools when unfocused
+         * - fixes ui freeze on other hardware accelerated softwares (chrome, vs code, ...)
+         * - fixes element selection in DevTools
+         */
+        app.disableHardwareAcceleration()
+
+        // Scheme must be registered before the app is ready
+        protocol.registerSchemesAsPrivileged([
+            { scheme: 'app', privileges: { secure: true, standard: true } },
+        ])
+    }
+
+    function exec(description: string, action: () => any) {
+        console.log(`[app]: ${ description }`)
+        return action()
+    }
+
+    const onQuit = () => {
+        gkl?.kill()
+        gameOverlay.quit()
+        /* fixes app.quit(): https://stackoverflow.com/a/75369483 */
+        ElectronWindowManager.getInstances().forEach(({ window }) => {
+            window.removeAllListeners()
+        })
+        destroyWinMouse()
+    }
+
+    function addEventListeners() {
+        app.whenReady().then(() => {
+            const credentialsDirPath = path.join(
+                app.getPath('userData'),
+                'credentials',
+            )
+            const googleCloudSpeechCredentialsFilePath = path.join(
+                credentialsDirPath,
+                'google-cloud-speech-credentials.json',
+            )
+            process.env.GOOGLE_APPLICATION_CREDENTIALS =
+                googleCloudSpeechCredentialsFilePath
+        })
+
+        app.on('ready', async () => {
+            if (isDevelopment) {
+                try {
+                    await ((installExtension as any).default as typeof installExtension)(
+                        VUEJS_DEVTOOLS,
+                    )
+                } catch (e) {
+                    console.error(
+                        'Vue Devtools failed to install:',
+                        (e as Error).toString(),
+                    )
+                }
+            }
+        })
+
+        app.on('activate', () => {
+            if (BrowserWindow.getAllWindows().length === 0) createWindows()
+        })
+
+        const handleQuit = (quitApp = false) => {
+            onQuit()
+            if (quitApp) {
+                app.quit()
+            }
+        }
+
+        process.on('message', (data) => {
+            if (process.platform === 'win32' && data === 'graceful-exit') {
+                handleQuit(true)
+            }
+        })
+        ;['SIGINT', 'SIGTERM', 'SIGQUIT'].forEach((signal) => {
+            process.on(signal, () => {
+                handleQuit(true)
+            })
+        })
+
+        app.on('before-quit', () => {
+            handleQuit()
+        })
+
+        app.on('window-all-closed', () => {
+            if (process.platform !== 'darwin') {
+                handleQuit(true)
+            }
+        })
+
+        app.on('web-contents-created', (_, webContents) => {
+            webContents.on('preload-error', (_, preloadPath, error) => {
+                console.error(
+                    `Preload script error:\nPath: ${ preloadPath }\nError:`,
+                    error,
+                )
+            })
+        })
+    }
+
+    function start() {
+        return Promise.all([
+            exec('Register app listeners', () => addEventListeners()),
+            exec('Configure app defaults', () => configureAppDefaults()),
+            exec('Register electron-pinia', () => registerElectronPinia()),
+            /* Check if app needs to run as admin before continuing */
+            exec('Register startup', () => registerElectronStartup()),
+        ]).then(async () => {
+            return Promise.all([
+                exec('Register updater', () => registerElectronUpdater()),
+                exec('Register debug', () => registerElectronDebug()),
+                exec('Bridge modules', () => bridgeModules()),
+                exec('Start Game Overlay', () => startGameOverlay()),
+                exec('Create tray', () => createTray()),
+                exec('Create windows', () => createWindows()),
+                exec('Start server', () => startAppServer()),
+                exec('Register display', () => registerElectronDisplay()),
+                exec('Register keybindings', () => registerElectronKeybinding()),
+                exec('Handle Cache', () => registerElectronCache()),
+            ])
+        })
+    }
+
+    return {
+        start,
+        isDevelopment,
+        exec,
+    }
 }
 
 export default App()
--- apps/app/src/modules/electron-startup/register.ts
+++ apps/app/src/modules/electron-startup/register.ts
@@ -2,61 +2,70 @@ import { watch } from 'vue'
 import { useSettingsStore } from '@/features/settings/store'
 import { app } from 'electron'
 import { exec } from 'child_process'
+import { Deferred } from '@packages/toolbox'
 
 function restartAsAdmin() {
-  if (process.platform === 'win32') {
-    const appPath = process.execPath
-    console.log(appPath)
-    exec(
-      `powershell -Command "Start-Process -FilePath '${appPath.replace(/'/g, "''")}' -Verb RunAs"`,
-      (error) => {
-        if (!error) {
-          app.quit()
-        } else {
-          console.error('Failed to restart as admin:', error)
-        }
-      },
-    )
-  } else {
-    console.warn('Admin restart is only implemented for Windows.')
-  }
+    if (process.platform === 'win32') {
+        const appPath = process.execPath
+        console.log(appPath)
+        exec(
+            `powershell -Command "Start-Process -FilePath '${ appPath.replace(/'/g, "''") }' -Verb RunAs"`,
+            (error) => {
+                if (!error) {
+                    app.quit()
+                } else {
+                    console.error('Failed to restart as admin:', error)
+                }
+            },
+        )
+    } else {
+        console.warn('Admin restart is only implemented for Windows.')
+    }
 }
 
 function isRunningAsAdmin(): boolean {
-  if (process.platform === 'win32') {
-    try {
-      const execSync = require('child_process').execSync
-      execSync('net session', { stdio: 'ignore' })
-      return true
-    } catch {
-      return false
+    if (process.platform === 'win32') {
+        try {
+            const execSync = require('child_process').execSync
+            execSync('net session', { stdio: 'ignore' })
+            return true
+        } catch {
+            return false
+        }
     }
-  }
-  return process.getuid?.() === 0
+    return process.getuid?.() === 0
 }
 
 const setLaunchOnStartup = (launchOnStartup: boolean) => {
-  console.log('[electron-startup] Launch on startup:', launchOnStartup)
-  app.setLoginItemSettings({
-    openAtLogin: launchOnStartup,
-    path: app.getPath('exe'),
-  })
+    console.log('[electron-startup] Launch on startup:', launchOnStartup)
+    app.setLoginItemSettings({
+        openAtLogin: launchOnStartup,
+        path: app.getPath('exe'),
+    })
 }
 
 export default () =>
-  app.whenReady().then(() => {
-    const settingsStore = useSettingsStore()
+    app.whenReady().then(() => {
+        const deferred = Deferred()
+        const settingsStore = useSettingsStore()
 
-    settingsStore.$whenReady().then(() => {
-      if (settingsStore.runAsAdmin && !isRunningAsAdmin()) {
-        restartAsAdmin()
-      }
+        settingsStore
+            .$whenReady()
+            .then(() => {
+                setLaunchOnStartup(settingsStore.launchOnStartup)
+                if (import.meta.env.PROD && settingsStore.runAsAdmin && !isRunningAsAdmin()) {
+                    restartAsAdmin()
+                } else {
+                    deferred.resolve(true)
+                }
+                watch(
+                    () => settingsStore.launchOnStartup,
+                    () => {
+                        setLaunchOnStartup(settingsStore.launchOnStartup)
+                    },
+                )
+            })
+            .catch((reason) => deferred.reject(reason))
+
+        return deferred.promise
     })
-    setLaunchOnStartup(settingsStore.launchOnStartup)
-    watch(
-      () => settingsStore.launchOnStartup,
-      () => {
-        setLaunchOnStartup(settingsStore.launchOnStartup)
-      },
-    )
-  })
