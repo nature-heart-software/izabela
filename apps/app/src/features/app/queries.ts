7c158f837d1d8cd864b8f904deb89c14ceed58e7
Wurielle <Wurielle@users.noreply.github.com>
2025-03-27 12:30:40 +0000

    style: prettier formatting

--- apps/app/src/features/app/queries.ts
+++ apps/app/src/features/app/queries.ts
@@ -1,5 +1,4 @@
 import { useQuery } from 'vue-query'
 
-export const useGetAppInfoQuery = () => useQuery('app-info', () =>
-    window.ElectronSI.getInfo(),
-)
+export const useGetAppInfoQuery = () =>
+  useQuery('app-info', () => window.ElectronSI.getInfo())
