9d897ce5f75c99a09d2da0873a3bc2eff1d884c5
Wurielle <Wurielle@users.noreply.github.com>
2025-03-28 10:23:01 +0000

    style: prettier formatting

--- apps/app/src/teams/messenger/electron/window.ts
+++ apps/app/src/teams/messenger/electron/window.ts
@@ -6,55 +6,56 @@ import electronMessengerWindow from '@/teams/messenger/modules/electron-messenge
 
 let window: BrowserWindow
 const createWindow = async (name: string): Promise<BrowserWindow> => {
-    window = new BrowserWindow({
-        show: false,
-        fullscreen: true,
-        transparent: true,
-        frame: false,
-        resizable: false,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: Boolean(
-                Number(import.meta.env.VITE_ELECTRON_NODE_INTEGRATION),
-            ),
-            contextIsolation: !Number(import.meta.env.VITE_ELECTRON_NODE_INTEGRATION),
-            sandbox: false,
-        },
-    })
-    ipcMain.registerBrowserWindow(name, window)
-    window.webContents.setMaxListeners(Infinity)
+  window = new BrowserWindow({
+    show: false,
+    fullscreen: true,
+    transparent: true,
+    frame: false,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: Boolean(
+        Number(import.meta.env.VITE_ELECTRON_NODE_INTEGRATION),
+      ),
+      contextIsolation: !Number(import.meta.env.VITE_ELECTRON_NODE_INTEGRATION),
+      sandbox: false,
+    },
+  })
+  ipcMain.registerBrowserWindow(name, window)
+  window.webContents.setMaxListeners(Infinity)
 
-    {
-        const primaryDisplay = screen.getPrimaryDisplay()
-        window.setBounds(primaryDisplay.bounds)
+  {
+    const primaryDisplay = screen.getPrimaryDisplay()
+    window.setBounds(primaryDisplay.bounds)
 
-        // https://github.com/electron/electron/issues/10078#issuecomment-331581160
-        window.setAlwaysOnTop(true, 'screen-saver', 1)
-        window.setVisibleOnAllWorkspaces(true)
-        window.setFullScreenable(false)
-        window.setMenu(null)
+    // https://github.com/electron/electron/issues/10078#issuecomment-331581160
+    window.setAlwaysOnTop(true, 'screen-saver', 1)
+    window.setVisibleOnAllWorkspaces(true)
+    window.setFullScreenable(false)
+    window.setMenu(null)
+  }
+
+  window.once('ready-to-show', () => {
+    electronMessengerWindow.start(window)
+  })
+
+  window.webContents.once('did-finish-load', () => {
+    if (import.meta.env.DEV) {
+      window.webContents.openDevTools({ mode: 'undocked' })
     }
+  })
 
-    window.once('ready-to-show', () => {
-        electronMessengerWindow.start(window)
-    })
+  const filePath = `./src/teams/${name}/index.html`
+  if (import.meta.env.VITE_DEV_SERVER_URL) {
+    await window.loadURL(
+      path.join(import.meta.env.VITE_DEV_SERVER_URL as string, filePath),
+    )
+  } else {
+    createProtocol('app')
+    await window.loadURL(`app://${filePath}`)
+  }
 
-
-    window.webContents.once('did-finish-load', () => {
-        if (import.meta.env.DEV) {
-            window.webContents.openDevTools({ mode: 'undocked' })
-        }
-    })
-
-    const filePath = `./src/teams/${ name }/index.html`
-    if (import.meta.env.VITE_DEV_SERVER_URL) {
-        await window.loadURL(path.join(import.meta.env.VITE_DEV_SERVER_URL as string, filePath))
-    } else {
-        createProtocol('app')
-        await window.loadURL(`app://${ filePath }`)
-    }
-
-    return window
+  return window
 }
 
 export default createWindow
