dd97e976f995a350c1ebe25a02b4796323437ce2
Wuriko <noahfenghom@gmail.com>
2025-03-28 11:22:24 +0100

    fix: fix alt shortcut not focusing window

--- apps/app/src/teams/messenger/electron/window.ts
+++ apps/app/src/teams/messenger/electron/window.ts
@@ -6,55 +6,55 @@ import electronMessengerWindow from '@/teams/messenger/modules/electron-messenge
 
 let window: BrowserWindow
 const createWindow = async (name: string): Promise<BrowserWindow> => {
-  window = new BrowserWindow({
-    show: false,
-    fullscreen: true,
-    transparent: true,
-    frame: false,
-    resizable: false,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: Boolean(
-        Number(import.meta.env.VITE_ELECTRON_NODE_INTEGRATION),
-      ),
-      contextIsolation: !Number(import.meta.env.VITE_ELECTRON_NODE_INTEGRATION),
-      sandbox: false,
-    },
-  })
-  ipcMain.registerBrowserWindow(name, window)
-  window.webContents.setMaxListeners(Infinity)
+    window = new BrowserWindow({
+        show: false,
+        fullscreen: true,
+        transparent: true,
+        frame: false,
+        resizable: false,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: Boolean(
+                Number(import.meta.env.VITE_ELECTRON_NODE_INTEGRATION),
+            ),
+            contextIsolation: !Number(import.meta.env.VITE_ELECTRON_NODE_INTEGRATION),
+            sandbox: false,
+        },
+    })
+    ipcMain.registerBrowserWindow(name, window)
+    window.webContents.setMaxListeners(Infinity)
 
-  {
-    const primaryDisplay = screen.getPrimaryDisplay()
-    window.setBounds(primaryDisplay.bounds)
+    {
+        const primaryDisplay = screen.getPrimaryDisplay()
+        window.setBounds(primaryDisplay.bounds)
 
-    // https://github.com/electron/electron/issues/10078#issuecomment-331581160
-    window.setAlwaysOnTop(true, 'screen-saver', 1)
-    window.setVisibleOnAllWorkspaces(true)
-    window.setFullScreenable(false)
-    window.setMenu(null)
-  }
-
-  window.once('ready-to-show', () => {
-    electronMessengerWindow.start(window)
-  })
-
-
-  window.webContents.once('did-finish-load', () => {
-    if (import.meta.env.DEV) {
-      window.webContents.openDevTools({ mode: 'undocked' })
+        // https://github.com/electron/electron/issues/10078#issuecomment-331581160
+        window.setAlwaysOnTop(true, 'screen-saver', 1)
+        window.setVisibleOnAllWorkspaces(true)
+        window.setFullScreenable(false)
+        window.setMenu(null)
     }
-  })
 
-  const filePath = `./src/teams/${ name }/index.html`
-  if (import.meta.env.VITE_DEV_SERVER_URL) {
-    await window.loadURL(path.join(import.meta.env.VITE_DEV_SERVER_URL as string, filePath))
-  } else {
-    createProtocol('app')
-    await window.loadURL(`app://${ filePath }`)
-  }
+    window.once('ready-to-show', () => {
+        electronMessengerWindow.start(window)
+    })
 
-  return window
+
+    window.webContents.once('did-finish-load', () => {
+        if (import.meta.env.DEV) {
+            window.webContents.openDevTools({ mode: 'undocked' })
+        }
+    })
+
+    const filePath = `./src/teams/${ name }/index.html`
+    if (import.meta.env.VITE_DEV_SERVER_URL) {
+        await window.loadURL(path.join(import.meta.env.VITE_DEV_SERVER_URL as string, filePath))
+    } else {
+        createProtocol('app')
+        await window.loadURL(`app://${ filePath }`)
+    }
+
+    return window
 }
 
 export default createWindow
