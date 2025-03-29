fcc4f6850612a6a81be3b931b3f2c35252a3c4c3
Wuriko <noahfenghom@gmail.com>
2025-03-29 13:32:34 +0100

    refactor: update synthesizeSpeech return type

--- apps/app/src/modules/izabela/IzabelaMessage.ts
+++ apps/app/src/modules/izabela/IzabelaMessage.ts
@@ -10,225 +10,245 @@ import objectHash from 'object-hash'
 import { IzabelaMessageEvent, IzabelaMessagePayload } from './types'
 
 export default (messagePayload: IzabelaMessagePayload) => {
-  const {
-    id: existingId,
-    engine: engineName,
-    excludeFromHistory,
-    disableAutoplay,
-    credentials,
-    payload,
-  } = messagePayload
-  const id = existingId || uuid()
-  const audio = new Audio()
-  const emitter = mitt()
-  const audioDownloaded = Deferred()
-  const audioLoaded = Deferred()
-  const playingMessageStore = usePlayingMessageStore()
-  let audioElements: (HTMLAudioElement | null)[] = []
-  let cancelled = false
-  if (!excludeFromHistory) {
-    const messageStore = useMessagesStore()
-    messageStore.$whenReady().then(() => {
-      messageStore.addToHistory(id, messagePayload)
-    })
-  }
-
-  function getCacheId() {
-    return `${id}-${objectHash(payload)}`
-  }
-
-  function on(event: IzabelaMessageEvent, callback: () => void): void {
-    emitter.on(event, callback)
-  }
-
-  function pause() {
-    audio.pause()
-  }
-
-  function resume() {
-    audio.play()
-  }
-
-  function cancel() {
-    cancelled = true
-    audio.pause()
-    audioElements.forEach((audioEl) => audioEl?.pause())
-    playingMessageStore.$patch({
-      id: null,
-      isPlaying: false,
-      progress: 0,
-    })
-    emitter.emit('ended')
-  }
-
-  function togglePlay() {
-    if (audio.paused) {
-      audio.play()
-    } else {
-      audio.pause()
-    }
-  }
-
-  async function play() {
-    const settingsStore = useSettingsStore()
-    await settingsStore.$whenReady()
-    Promise.map(settingsStore.audioOutputs, async (deviceLabel: string) => {
-      // TODO: Some optimisation possible here
-      let mediaDevice
-
-      try {
-        mediaDevice = await getMediaDeviceByLabel(deviceLabel)
-      } catch (error) {
-        console.error(error)
-        return null
-      }
-      if (mediaDevice) {
-        const audioElement: any = document.createElement('audio')
-        audioElement.src = audio.src
-
-        try {
-          await audioElement.setSinkId(mediaDevice.deviceId)
-        } catch (error) {
-          console.error(error)
-          return null
-        }
-        return audioElement
-      }
-      return null
-    })
-      .then((res: typeof audioElements) => {
-        if (cancelled) return
-        if (!settingsStore.playSpeechOnDefaultPlaybackDevice) {
-          audio.volume = 0
-        }
-        audio.play()
-        audioElements = res
-        audioElements.forEach((audioEl) => audioEl && audioEl.play())
-      })
-      .catch(console.error)
-  }
-
-  function isReady() {
-    return Promise.all([audioDownloaded.promise, audioLoaded.promise])
-  }
-
-  async function downloadAudio() {
-    if (typeof window) {
-      const { ElectronFilesystem } = window
-      const cachedAudio = await ElectronFilesystem.getCachedAudio(getCacheId())
-      if (cachedAudio) {
-        const res = await fetch(cachedAudio)
-        const blob = await res.blob()
-        if (blob) {
-          audioDownloaded.resolve(true)
-          return Promise.resolve(blob)
-        }
-      }
-    }
-    // TODO: change depending on engine
-    const engine = getEngineById(engineName)
-    if (!engine) return Promise.reject(new Error('Izabela Message: Selected engine was not found'))
-    return engine
-      .synthesizeSpeech({
+    const {
+        id: existingId,
+        engine: engineName,
+        excludeFromHistory,
+        disableAutoplay,
         credentials,
         payload,
-      })
-      .then((res) => {
-        audioDownloaded.resolve(true)
-        const blob = 'data' in res ? res.data : res
-        cacheAudio(blob)
-        return Promise.resolve(blob)
-      })
-  }
-
-  async function cacheAudio(blob: Blob) {
-    if (typeof window) {
-      const { ElectronFilesystem } = window
-      const base64 = await blobToBase64(blob)
-      if (base64) ElectronFilesystem.cacheAudio(getCacheId(), base64)
+    } = messagePayload
+    const id = existingId || uuid()
+    const audio = new Audio()
+    const emitter = mitt()
+    const audioDownloaded = Deferred()
+    const audioLoaded = Deferred()
+    const playingMessageStore = usePlayingMessageStore()
+    let audioElements: (HTMLAudioElement | null)[] = []
+    let cancelled = false
+    if (!excludeFromHistory) {
+        const messageStore = useMessagesStore()
+        messageStore.$whenReady().then(() => {
+            messageStore.addToHistory(id, messagePayload)
+        })
     }
-  }
 
-  function loadAudio(blob: Blob) {
-    audio.src = URL.createObjectURL(blob)
-    audio.load()
-  }
+    function getCacheId() {
+        return `${ id }-${ objectHash(payload) }`
+    }
 
-  function getAudioProgress() {
-    return audio.currentTime / audio.duration
-  }
+    function on(event: IzabelaMessageEvent, callback: () => void): void {
+        emitter.on(event, callback)
+    }
 
-  function addEventListeners() {
-    audio.addEventListener('timeupdate', () => {
-      if (cancelled) return
-      playingMessageStore.$patch({
-        progress: audio.currentTime / audio.duration || 0,
-      })
-    })
-    audio.addEventListener('ended', () => {
-      if (cancelled) return
-      playingMessageStore.$patch({
-        id: null,
-        isPlaying: false,
-        progress: 0,
-      })
-    })
-    audio.addEventListener('play', () => {
-      if (cancelled) return
-      playingMessageStore.$patch({
+    function pause() {
+        audio.pause()
+    }
+
+    function resume() {
+        audio.play()
+    }
+
+    function cancel() {
+        cancelled = true
+        audio.pause()
+        audioElements.forEach((audioEl) => audioEl?.pause())
+        playingMessageStore.$patch({
+            id: null,
+            isPlaying: false,
+            progress: 0,
+        })
+        emitter.emit('ended')
+    }
+
+    function togglePlay() {
+        if (audio.paused) {
+            audio.play()
+        } else {
+            audio.pause()
+        }
+    }
+
+    async function play() {
+        const settingsStore = useSettingsStore()
+        await settingsStore.$whenReady()
+        Promise.map(settingsStore.audioOutputs, async (deviceLabel: string) => {
+            // TODO: Some optimisation possible here
+            let mediaDevice
+
+            try {
+                mediaDevice = await getMediaDeviceByLabel(deviceLabel)
+            } catch (error) {
+                console.error(error)
+                return null
+            }
+            if (mediaDevice) {
+                const audioElement: any = document.createElement('audio')
+                audioElement.src = audio.src
+
+                try {
+                    await audioElement.setSinkId(mediaDevice.deviceId)
+                } catch (error) {
+                    console.error(error)
+                    return null
+                }
+                return audioElement
+            }
+            return null
+        })
+            .then((resolvedAudioElements: typeof audioElements) => {
+                if (cancelled) return
+                if (!settingsStore.playSpeechOnDefaultPlaybackDevice) {
+                    audio.volume = 0
+                }
+                audio.play()
+                audioElements = resolvedAudioElements
+                audioElements.forEach((audioEl) => audioEl && audioEl.play())
+            })
+            .catch(console.error)
+    }
+
+    function isReady() {
+        return Promise.all([audioDownloaded.promise, audioLoaded.promise])
+    }
+
+    async function downloadAudio() {
+        if (typeof window) {
+            const { ElectronFilesystem } = window
+            const cachedAudio = await ElectronFilesystem.getCachedAudio(getCacheId())
+            if (cachedAudio) {
+                const res = await fetch(cachedAudio)
+                const blob = await res.blob()
+                if (blob) {
+                    audioDownloaded.resolve(true)
+                    return Promise.resolve(blob)
+                }
+            }
+        }
+        // TODO: change depending on engine
+        const engine = getEngineById(engineName)
+        if (!engine) return Promise.reject(new Error('Izabela Message: Selected engine was not found'))
+        return engine
+            .synthesizeSpeech({
+                credentials,
+                payload,
+            })
+            .then((src) => {
+                audioDownloaded.resolve(true)
+                cacheAudio(src)
+                return Promise.resolve(src)
+            })
+    }
+
+    async function cacheAudio(data: Blob | MediaSource) {
+        if (typeof window !== 'undefined') {
+            const { ElectronFilesystem } = window
+
+            if (data instanceof Blob) {
+                const base64 = await blobToBase64(data)
+                if (base64) {
+                    ElectronFilesystem.cacheAudio(getCacheId(), base64)
+                }
+            } else if (data instanceof MediaSource) {
+                throw 'need to implement this first'
+                // data.addEventListener('sourceopen', () => {
+                //   // At this point, the MediaSource is ready to accept data
+                //
+                //   // You may want to append the audio data to the SourceBuffer (this assumes you've got audio data to append)
+                //   const sourceBuffer = data.addSourceBuffer('audio/mpeg'); // Use appropriate MIME type for your data
+                //   // Here you might want to append chunks of data if streaming, not just one Blob
+                //   // Example: sourceBuffer.appendBuffer(someChunkOfAudio);
+                // });
+                //
+                // // Optionally: You can listen to the audio's `canplaythrough` event or `play` event to ensure it's ready for playback
+                // audioElement.addEventListener('canplaythrough', () => {
+                //   // Audio element is ready to play
+                //   console.log("Audio is ready for playback");
+                // });
+            }
+        }
+    }
+
+    function loadAudio(blob: Blob | MediaSource) {
+        audio.src = URL.createObjectURL(blob)
+        audio.load()
+    }
+
+    function getAudioProgress() {
+        return audio.currentTime / audio.duration
+    }
+
+    function addEventListeners() {
+        audio.addEventListener('timeupdate', () => {
+            if (cancelled) return
+            playingMessageStore.$patch({
+                progress: audio.currentTime / audio.duration || 0,
+            })
+        })
+        audio.addEventListener('ended', () => {
+            if (cancelled) return
+            playingMessageStore.$patch({
+                id: null,
+                isPlaying: false,
+                progress: 0,
+            })
+        })
+        audio.addEventListener('play', () => {
+            if (cancelled) return
+            playingMessageStore.$patch({
+                id,
+                isPlaying: true,
+            })
+        })
+        audio.addEventListener('pause', () => {
+            if (cancelled) return
+            playingMessageStore.$patch({
+                id,
+                isPlaying: false,
+            })
+        })
+        audio.addEventListener('canplaythrough', () => {
+            emitter.emit('canplaythrough')
+            audioLoaded.resolve(true)
+        })
+        audio.addEventListener('started', () => emitter.emit('started'))
+        audio.addEventListener('ended', () => emitter.emit('ended'))
+        audio.addEventListener('progress', () => emitter.emit('progress', getAudioProgress()))
+        audio.addEventListener('error', (e) => onError(e))
+    }
+
+    function onError(e: ErrorEvent) {
+        emitter.emit('error')
+        audioDownloaded.reject(e)
+        audioLoaded.reject(e)
+    }
+
+    if (!disableAutoplay) {
+        addEventListeners()
+        downloadAudio()
+            .then((blob) => {
+                loadAudio(blob)
+            })
+            .catch((reason) => onError(reason))
+    }
+
+    return {
         id,
-        isPlaying: true,
-      })
-    })
-    audio.addEventListener('pause', () => {
-      if (cancelled) return
-      playingMessageStore.$patch({
-        id,
-        isPlaying: false,
-      })
-    })
-    audio.addEventListener('canplaythrough', () => {
-      emitter.emit('canplaythrough')
-      audioLoaded.resolve(true)
-    })
-    audio.addEventListener('started', () => emitter.emit('started'))
-    audio.addEventListener('ended', () => emitter.emit('ended'))
-    audio.addEventListener('progress', () => emitter.emit('progress', getAudioProgress()))
-    audio.addEventListener('error', (e) => onError(e))
-  }
-
-  function onError(e: ErrorEvent) {
-    emitter.emit('error')
-    audioDownloaded.reject(e)
-    audioLoaded.reject(e)
-  }
-
-  if (!disableAutoplay) {
-    addEventListeners()
-    downloadAudio()
-      .then((blob) => {
-        loadAudio(blob)
-      })
-      .catch((reason) => onError(reason))
-  }
-
-  return {
-    id,
-    isReady,
-    play,
-    on,
-    downloadAudio,
-    pause,
-    resume,
-    cancel,
-    togglePlay,
-    getSocketPayload: () => {
-      const { credentials: _, ...rest } = messagePayload
-      return {
-        ...rest,
-        id,
-        timestamp: new Date().toISOString(),
-      }
-    },
-  }
+        isReady,
+        play,
+        on,
+        downloadAudio,
+        pause,
+        resume,
+        cancel,
+        togglePlay,
+        getSocketPayload: () => {
+            const { credentials: _, ...rest } = messagePayload
+            return {
+                ...rest,
+                id,
+                timestamp: new Date().toISOString(),
+            }
+        },
+    }
 }
--- apps/app/src/plugins/speech-engines/custom/register.ts
+++ apps/app/src/plugins/speech-engines/custom/register.ts
@@ -8,41 +8,42 @@ import { getProperty, setProperty } from './store'
 
 const getSelectedVoice = () => getProperty('selectedVoice')
 registerEngine({
-  id: ENGINE_ID,
-  name: ENGINE_NAME,
-  category: 'other',
-  getSelectedVoice,
-  getVoiceName,
-  hasCredentials() {
-    return getProperty('endpoint')
-  },
-  getCredentials() {
-    return {
-      apiKey: getProperty('apiKey', true),
-    }
-  },
-  getPayload({ text, translatedText, voice: v }) {
-    const voice = v || getSelectedVoice()
-    return {
-      text: translatedText || text,
-      voice,
-    }
-  },
-  getLanguageCode(voice) {
-    return (voice || getSelectedVoice()).languageCode || DEFAULT_LANGUAGE_CODE
-  },
-  synthesizeSpeech({ payload, credentials }) {
-    const endpoint = getProperty('endpoint')
-    return axios.post<Blob>(
-      `${endpoint.endsWith('/') ? endpoint.slice(0, -1) : endpoint}/synthesize-speech`,
-      {
-        credentials,
-        payload,
-      },
-      { responseType: 'blob' },
-    )
-  },
-  voiceSelectComponent: NvVoiceSelect,
-  settingsComponent: NvSettings,
-  store: { setProperty, getProperty },
+    id: ENGINE_ID,
+    name: ENGINE_NAME,
+    category: 'other',
+    getSelectedVoice,
+    getVoiceName,
+    hasCredentials() {
+        return getProperty('endpoint')
+    },
+    getCredentials() {
+        return {
+            apiKey: getProperty('apiKey', true),
+        }
+    },
+    getPayload({ text, translatedText, voice: v }) {
+        const voice = v || getSelectedVoice()
+        return {
+            text: translatedText || text,
+            voice,
+        }
+    },
+    getLanguageCode(voice) {
+        return (voice || getSelectedVoice()).languageCode || DEFAULT_LANGUAGE_CODE
+    },
+    synthesizeSpeech({ payload, credentials }) {
+        const endpoint = getProperty('endpoint')
+        return axios.post<Blob>(
+            `${ endpoint.endsWith('/') ? endpoint.slice(0, -1) : endpoint }/synthesize-speech`,
+            {
+                credentials,
+                payload,
+            },
+            { responseType: 'blob' },
+        )
+            .then((res) => res.data)
+    },
+    voiceSelectComponent: NvVoiceSelect,
+    settingsComponent: NvSettings,
+    store: { setProperty, getProperty },
 })
