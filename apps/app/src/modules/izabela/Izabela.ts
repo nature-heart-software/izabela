45c1b9b60b19034743886f14e0b9a251c1cdbd09
Wurielle <noahfenghom@gmail.com>
2025-04-17 14:03:10 +0200

    wip: prepare audio streaming from server

--- apps/app/src/modules/izabela/Izabela.ts
+++ apps/app/src/modules/izabela/Izabela.ts
@@ -72,8 +72,19 @@ export default () => {
     }
   }
 
-  function createMessage(messagePayload: IzabelaMessagePayload) {
-    return IzabelaMessage(messagePayload)
+  function createMessage(messagePayload: Partial<IzabelaMessagePayload> & Pick<IzabelaMessagePayload, 'engine'>) {
+    return IzabelaMessage({
+      message: '',
+      originalMessage: '',
+      translatedMessage: null,
+      translatedFrom: null,
+      translatedTo: null,
+      payload: {},
+      credentials: {},
+      command: null,
+      voice: null,
+      ...messagePayload
+    })
   }
 
   function queueMessage(message: ReturnType<typeof IzabelaMessage>) {
@@ -90,6 +101,21 @@ export default () => {
     }
     return playMessage(message)
   }
+
+  function stream(url: string): ReturnType<typeof IzabelaMessage> {
+    const message = createMessage({
+      engine: 'stream',
+      payload: {
+        url,
+      },
+    })
+    if (currentlyPlayingMessage) {
+      return queueMessage(message)
+    }
+    return playMessage(message)
+  }
+
+  socket.on('stream', stream)
 
   return {
     say,
--- apps/app/src/modules/izabela/types.ts
+++ apps/app/src/modules/izabela/types.ts
@@ -15,7 +15,7 @@ export interface IzabelaMessage {
   translatedFrom: string | null
   translatedTo: string | null
   command: string | null
-  engine: SpeechEngine['id']
+  engine: SpeechEngine['id'] & {} | 'stream'
   voice: any
   excludeFromHistory?: boolean
   disableAutoplay?: boolean
