8995f42794caf3a7f4c74853c2d548bc33ebd01e
Wurielle <Wurielle@users.noreply.github.com>
2025-05-09 09:00:07 +0000

    style: prettier formatting

--- apps/app/src/plugins/translation-engines/google-translate/NvSettings.vue
+++ apps/app/src/plugins/translation-engines/google-translate/NvSettings.vue
@@ -1,27 +1,23 @@
 <template>
-    <NvStack :spacing="size === 'sm' ? 4 : 5">
-      <NvFormItem label="From">
-        <NvTranslateFromSelect
-          :modelValue="getProperty('translateFrom')"
-          @update:modelValue="(value) => setProperty('translateFrom', value)"
-        />
-      </NvFormItem>
-      <NvDivider direction="horizontal" />
-      <NvFormItem label="To">
-        <NvTranslateToSelect
-          :modelValue="getProperty('translateTo')"
-          @update:modelValue="(value) => setProperty('translateTo', value)"
-        />
-      </NvFormItem>
-    </NvStack>
+  <NvStack :spacing="size === 'sm' ? 4 : 5">
+    <NvFormItem label="From">
+      <NvTranslateFromSelect
+        :modelValue="getProperty('translateFrom')"
+        @update:modelValue="(value) => setProperty('translateFrom', value)"
+      />
+    </NvFormItem>
+    <NvDivider direction="horizontal" />
+    <NvFormItem label="To">
+      <NvTranslateToSelect
+        :modelValue="getProperty('translateTo')"
+        @update:modelValue="(value) => setProperty('translateTo', value)"
+      />
+    </NvFormItem>
+  </NvStack>
 </template>
 <script lang="ts" setup>
 import { PropType } from 'vue'
-import {
-  NvDivider,
-  NvFormItem,
-  NvStack,
-} from '@packages/ui'
+import { NvDivider, NvFormItem, NvStack } from '@packages/ui'
 import NvTranslateFromSelect from './NvTranslateFromSelect.vue'
 import NvTranslateToSelect from './NvTranslateToSelect.vue'
 import { store } from './store.ts'
@@ -33,7 +29,5 @@ const props = defineProps({
   },
   form: Object,
 })
-const { getProperty, setProperty } =
-  store.useStoreOrForm(props.form)
-
+const { getProperty, setProperty } = store.useStoreOrForm(props.form)
 </script>
--- apps/app/src/plugins/translation-engines/google-translate/bridge.ts
+++ apps/app/src/plugins/translation-engines/google-translate/bridge.ts
@@ -1,17 +1,20 @@
 import { bridge } from '@packages/electron-bridger'
 import { electronModuleName } from './shared.ts'
-import { translate } from '@vitalets/google-translate-api';
+import { translate } from '@vitalets/google-translate-api'
 
 console.log(electronModuleName)
 bridge.register([
-  [electronModuleName, () => ({
-    async translate({text, from, to}: any) {
-      try {
-        const response = await translate(text, { from, to });
-        return response.text
-      } catch (e) {
-        return text
-      }
-    }
-  })],
+  [
+    electronModuleName,
+    () => ({
+      async translate({ text, from, to }: any) {
+        try {
+          const response = await translate(text, { from, to })
+          return response.text
+        } catch (e) {
+          return text
+        }
+      },
+    }),
+  ],
 ])
