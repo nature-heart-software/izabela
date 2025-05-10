82ce8418610e377b487bdd142cbf2da602ced4f6
Wurielle <Wurielle@users.noreply.github.com>
2025-05-10 19:00:58 +0000

    style: prettier formatting

--- packages/ui/src/tokens/light.ts
+++ packages/ui/src/tokens/light.ts
@@ -6,88 +6,88 @@ export default {
   button: {
     default: {
       backgroundColor: colors.white,
-      borderColor: colors.gray["20"],
+      borderColor: colors.gray['20'],
       hover: {
-        backgroundColor: colors.gray["10"],
+        backgroundColor: colors.gray['10'],
       },
       active: {
-        backgroundColor: colors.gray["30"],
+        backgroundColor: colors.gray['30'],
       },
       focus: {
-        boxShadow: colors.gray["10"],
+        boxShadow: colors.gray['10'],
       },
       selected: {
-        backgroundColor: colors.gray["20"],
-      }
+        backgroundColor: colors.gray['20'],
+      },
     },
     active: {
       backgroundColor: colors.white,
-      borderColor: colors.gray["100"],
+      borderColor: colors.gray['100'],
       hover: {
-        backgroundColor: colors.gray["10"],
+        backgroundColor: colors.gray['10'],
       },
       active: {
-        backgroundColor: colors.gray["30"],
+        backgroundColor: colors.gray['30'],
       },
       focus: {
-        boxShadow: colors.gray["10"],
+        boxShadow: colors.gray['10'],
       },
       selected: {
-        backgroundColor: colors.gray["20"],
-      }
+        backgroundColor: colors.gray['20'],
+      },
     },
     plain: {
       color: colors.white,
-      backgroundColor: colors.gray["100"],
-      borderColor: colors.gray["100"],
+      backgroundColor: colors.gray['100'],
+      borderColor: colors.gray['100'],
       hover: {
-        borderColor: colors.gray["90"],
-        backgroundColor: colors.gray["90"],
+        borderColor: colors.gray['90'],
+        backgroundColor: colors.gray['90'],
       },
       active: {
-        borderColor: colors.gray["70"],
-        backgroundColor: colors.gray["70"],
+        borderColor: colors.gray['70'],
+        backgroundColor: colors.gray['70'],
       },
       focus: {
-        boxShadow: colors.gray["70"],
+        boxShadow: colors.gray['70'],
       },
       selected: {
-        borderColor: colors.gray["80"],
-        backgroundColor: colors.gray["80"],
-      }
+        borderColor: colors.gray['80'],
+        backgroundColor: colors.gray['80'],
+      },
     },
     ghost: {
-      backgroundColor: "transparent",
-      borderColor: "transparent",
+      backgroundColor: 'transparent',
+      borderColor: 'transparent',
       hover: {
-        backgroundColor: colors.gray["10"],
+        backgroundColor: colors.gray['10'],
       },
       active: {
-        backgroundColor: colors.gray["30"],
+        backgroundColor: colors.gray['30'],
       },
       focus: {
-        boxShadow: colors.gray["10"],
+        boxShadow: colors.gray['10'],
       },
       selected: {
-        backgroundColor: colors.gray["20"],
-      }
+        backgroundColor: colors.gray['20'],
+      },
     },
-    "ghost-alt": {
-      backgroundColor: "transparent",
-      borderColor: "transparent",
+    'ghost-alt': {
+      backgroundColor: 'transparent',
+      borderColor: 'transparent',
       hover: {
-        backgroundColor: colors.gray["20"],
+        backgroundColor: colors.gray['20'],
       },
       active: {
-        backgroundColor: colors.gray["30"],
+        backgroundColor: colors.gray['30'],
       },
       focus: {
-        boxShadow: colors.gray["10"],
+        boxShadow: colors.gray['10'],
       },
       selected: {
         boxShadow: 'boxShadow.DEFAULT',
-        backgroundColor: colors.gray["0"],
-      }
-    }
-  }
+        backgroundColor: colors.gray['0'],
+      },
+    },
+  },
 }
