646d498dbedc38e516d5441e1d25c6b2235334f8
Wurielle <Wurielle@users.noreply.github.com>
2025-11-07 21:49:54 +0000

    style: prettier formatting

--- apps/app/src/modules/node-mouse/index.ts
+++ apps/app/src/modules/node-mouse/index.ts
@@ -1,4 +1,4 @@
-import { mouse, } from '@nut-tree-fork/nut-js'
+import { mouse } from '@nut-tree-fork/nut-js'
 import mitt from 'mitt'
 import throttle from 'lodash/throttle'
 
@@ -9,13 +9,19 @@ let stop = true
 export function startMouse(throttleMs: number = 0) {
   if (typeof window !== 'undefined') return null
   stop = false
-  const check = throttle(() => mouse.getPosition().then(mousePosition => {
-    if (stop) return
-    mouseEventEmitter.emit('move', mousePosition)
-    check()
-  }), throttleMs)
+  const check = throttle(
+    () =>
+      mouse.getPosition().then((mousePosition) => {
+        if (stop) return
+        mouseEventEmitter.emit('move', mousePosition)
+        check()
+      }),
+    throttleMs,
+  )
   check()
   return mouseEventEmitter
 }
 
-export function stopMouse(){ stop = true }
+export function stopMouse() {
+  stop = true
+}
